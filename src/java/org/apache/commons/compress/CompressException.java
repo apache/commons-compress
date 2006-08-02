be36da7838d75225b13c5ca6377b97ba61f20abb
Torsten Curdt <tcurdt@apache.org>
2006-08-02 04:45:26 +0000

    forgot a few files to add: draft 8 from C. Grobmeier, grant has been received, cheers
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/compress/trunk@427890 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/compress/CompressException.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2002,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.compress;
+
+/**
+ * Exception occurs when a exception within 
+ * the compress actions occurs.
+ */
+public class CompressException extends PackableObjectException {
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 3257005449554507057L;
+
+	/**
+	 * Calls the IOException constructor
+	 */
+	public CompressException() {
+		super();
+	}
+
+	/**
+	 * Calls the IOException constructor with a message
+	 * @param message the message
+	 */
+	public CompressException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * Calls the IOException constructor with a message
+	 * and fills the stacktrace with the stacktrace of 
+	 * an exception
+	 * 
+	 * @param message the message
+	 * @param e the exception 
+	 */
+	public CompressException(String message, Exception e) {
+		super(message);
+		this.initCause(e);
+	}
+}
---
+++ src/java/org/apache/commons/compress/CompressUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2002,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.compress;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Utils for Compress.
+ */
+public class CompressUtils {
+	/*
+	 * This class should not be constructed
+	 */
+	private CompressUtils() {
+		// unused
+	}
+	
+	/**
+     * Copy bytes from an <code>InputStream</code> to an <code>OutputStream</code>.
+     */
+    public static void copy( final InputStream input,
+                              final OutputStream output )
+        throws IOException {
+        final byte[] buffer = new byte[ 8024 ];
+        int n = 0;
+        while( -1 != ( n = input.read( buffer ) ) ) {
+            output.write( buffer, 0, n );
+        }
+    }
+    
+    /**
+     * Compares one byte array to another
+	 * @param source- the array to compare to 
+	 * @param headerBytes - the bytearray match
+	 */
+	public static boolean compareByteArrays(byte[] source, byte[] match) {
+		int i = 0;
+		while(source.length < i || i < match.length ) {
+			if(source[i] != match[i]) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+}
