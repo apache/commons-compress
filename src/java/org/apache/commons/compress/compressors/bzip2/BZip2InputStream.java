e3f9bf21b5fb41b8de37fed6f2a778de3845da8e
Sebastian Bazley <sebb@apache.org>
2008-07-06 00:15:52 +0000

    Remove executable properties Also replace char with int in BZip2InputStream as char cannot be compared with -1
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/compress/trunk@674246 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/compress/compressors/bzip2/BZip2InputStream.java
+++ src/java/org/apache/commons/compress/compressors/bzip2/BZip2InputStream.java
@@ -451,10 +451,10 @@ public class BZip2InputStream
             while( m_bsLive < 1 )
             {
                 int zzi;
-                char thech = 0;
+                int thech = 0;
                 try
                 {
-                    thech = (char)m_input.read();
+                    thech = m_input.read();
                 }
                 catch( IOException e )
                 {
@@ -516,10 +516,10 @@ public class BZip2InputStream
                         while( m_bsLive < 1 )
                         {
                             int zzi;
-                            char thech = 0;
+                            int thech = 0;
                             try
                             {
-                                thech = (char)m_input.read();
+                                thech = m_input.read();
                             }
                             catch( IOException e )
                             {
@@ -673,10 +673,10 @@ public class BZip2InputStream
     {
         while( m_bsLive < n )
         {
-            char ch = 0;
+            int ch = 0;
             try
             {
-                ch = (char)m_input.read();
+                ch = m_input.read();
             }
             catch( final IOException ioe )
             {
