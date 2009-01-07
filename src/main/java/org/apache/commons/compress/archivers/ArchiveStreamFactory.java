84c83d4f4b77f8d8be353bdf8a231f0f655e8fdb
Torsten Curdt <tcurdt@apache.org>
2009-01-07 13:16:01 +0000

    applied patch from Christian Grobmeier
    https://issues.apache.org/jira/browse/SANDBOX-262
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/compress/trunk@732325 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/ArchiveStreamFactory.java
+++ src/main/java/org/apache/commons/compress/archivers/ArchiveStreamFactory.java
@@ -21,12 +21,6 @@ package org.apache.commons.compress.archivers;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 import org.apache.commons.compress.archivers.ar.ArArchiveInputStream;
 import org.apache.commons.compress.archivers.ar.ArArchiveOutputStream;
@@ -36,97 +30,44 @@ import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
-import org.apache.commons.compress.utils.ReflectionUtils;
+
+/**
+ * Factory to create Archive[In|Out]putStreams from names
+ * or the first bytes of the InputStream. In order add other
+ * implementations you should extend ArchiveStreamFactory
+ * and override the appropriate methods (and call their implementation
+ * from super of course)
+ * 
+ * TODO add example here 
+ * 
+ */
 
 public class ArchiveStreamFactory {
 
-	final Map inputStreamClasses = new HashMap();
-	final Map outputStreamClasses = new HashMap();
-	
-	public ArchiveStreamFactory() throws ArchiveException {
-		registerArchiveInputStream("zip", ZipArchiveInputStream.class);
-		registerArchiveOutputStream("zip", ZipArchiveOutputStream.class);
-
-        registerArchiveInputStream("tar", TarArchiveInputStream.class);
-        registerArchiveOutputStream("tar", TarArchiveOutputStream.class);
-
-        registerArchiveInputStream("ar", ArArchiveInputStream.class);
-        registerArchiveOutputStream("ar", ArArchiveOutputStream.class);
-
-        registerArchiveInputStream("jar", JarArchiveInputStream.class);		
-        registerArchiveOutputStream("jar", JarArchiveOutputStream.class);
-	}
-	
-	
-	public void registerArchiveInputStream( final String name, final Class stream ) throws ArchiveException {
-		if (ArchiveInputStream.class.isAssignableFrom(stream) && !(stream.isInterface())) {
-			inputStreamClasses.put(name, stream);
-        } else {
-            throw new ArchiveException("Archive does not implement the ArchiveInputStream interface.");
-        }	
-	}
-
-	public void registerArchiveOutputStream( final String name, final Class stream ) throws ArchiveException {
-		ReflectionUtils.registerClazz(outputStreamClasses, name, ArchiveOutputStream.class, stream);		
-		if (ArchiveOutputStream.class.isAssignableFrom(stream) && !(stream.isInterface())) {
-			outputStreamClasses.put(name, stream);
-        } else {
-            throw new ArchiveException("Archive does not implement the ArchiveOutputStream interface.");
+    public ArchiveInputStream createArchiveInputStream( final String archiverName, final InputStream in ) throws ArchiveException {
+    	if ("ar".equalsIgnoreCase(archiverName)) {
+            return new ArArchiveInputStream(in);
+        } else if("zip".equalsIgnoreCase(archiverName)) {
+        	return new ZipArchiveInputStream(in);
+        } else if("tar".equalsIgnoreCase(archiverName)) {
+        	return new TarArchiveInputStream(in);
+        } else if("jar".equalsIgnoreCase(archiverName)) {
+        	return new JarArchiveInputStream(in);
         }
-	}
-	
-    public ArchiveInputStream createArchiveInputStream( final String archiverName, final InputStream out ) throws ArchiveException {
-        try {
-            final Class clazz = (Class) inputStreamClasses.get(archiverName);
-
-            if (clazz == null) {
-            	throw new ArchiveException("ArchiverFactory could not create instance");
-            }
-
-            final Class[] params = { InputStream.class };
-            final Constructor constructor = clazz.getConstructor(params);
-            final Object[] initargs = { out };
-            return (ArchiveInputStream) constructor.newInstance(initargs);
-        } catch (InstantiationException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (IllegalAccessException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (SecurityException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (NoSuchMethodException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (IllegalArgumentException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (InvocationTargetException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        }
+    	return null;
     }
 
     public ArchiveOutputStream createArchiveOutputStream( final String archiverName, final OutputStream out ) throws ArchiveException {
-        try {
-            final Class clazz = (Class) outputStreamClasses.get(archiverName);
-            
-            if (clazz == null) {
-            	throw new ArchiveException("ArchiverFactory could not create instance");
-            }
-            
-            final Class[] params = { OutputStream.class };
-            final Constructor constructor = clazz.getConstructor(params);
-            final Object[] initargs = { out };
-            return (ArchiveOutputStream) constructor.newInstance(initargs);
-        } catch (InstantiationException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (IllegalAccessException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (SecurityException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (NoSuchMethodException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (IllegalArgumentException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
-        } catch (InvocationTargetException e) {
-            throw new ArchiveException("ArchiverFactory could not create instance", e);
+    	if ("ar".equalsIgnoreCase(archiverName)) {
+            return new ArArchiveOutputStream(out);
+        } else if("zip".equalsIgnoreCase(archiverName)) {
+        	return new ZipArchiveOutputStream(out);
+        } else if("tar".equalsIgnoreCase(archiverName)) {
+        	return new TarArchiveOutputStream(out);
+        } else if("jar".equalsIgnoreCase(archiverName)) {
+        	return new JarArchiveOutputStream(out);
         }
+    	return null;
     }
 
     public ArchiveInputStream createArchiveInputStream( final InputStream input ) throws IOException {
@@ -134,36 +75,18 @@ public class ArchiveStreamFactory {
 		final byte[] signature = new byte[12];
 		input.mark(signature.length);
 		input.read(signature);
-		// reset not supported exception?
+		// TODO if reset is not supported pass on the IOException or return null?
 		input.reset();
 
-//		for (int i = 0; i < signature.length; i++) {
-//			System.out.print(Integer.toHexString(signature[i]));
-//			System.out.print(",");
-//		}
-//		System.out.println("");
-		
-		for (Iterator it = inputStreamClasses.values().iterator(); it.hasNext();) {
-			final Class clazz = (Class) it.next();
-			try {
-				final Method method = clazz.getMethod("matches", new Class[] { byte[].class });
-				
-				final Object result = method.invoke(null, new Object[] { signature } );
-				
-				if (result.equals(Boolean.TRUE)) {
-		            final Class[] params = { InputStream.class };
-		            final Constructor constructor = clazz.getConstructor(params);
-		            final Object[] initargs = { input };
-		            return (ArchiveInputStream) constructor.newInstance(initargs);					
-				}
-			} catch (SecurityException e) {
-			} catch (NoSuchMethodException e) {
-			} catch (IllegalArgumentException e) {
-			} catch (IllegalAccessException e) {
-			} catch (InvocationTargetException e) {
-			} catch (InstantiationException e) {
-			}
-		}
+		if(ZipArchiveInputStream.matches(signature)) {
+			return new ZipArchiveInputStream(input);
+		} else if(JarArchiveInputStream.matches(signature)) {
+			return new JarArchiveInputStream(input);
+		} else if(TarArchiveInputStream.matches(signature)) {
+			return new TarArchiveInputStream(input);
+		} else if(ArArchiveInputStream.matches(signature)) {
+			return new ArArchiveInputStream(input);
+		} 
 		return null;
 	}
 }
