ba84c05d2f20442c529fa585dac3d79e4f190163
Torsten Curdt <tcurdt@apache.org>
2009-01-08 11:13:50 +0000

    applied patch from Christian Grobmeier
    updated bzip2 streams
    https://issues.apache.org/jira/browse/SANDBOX-195
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/compress/trunk@732685 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/compressors/bzip2/BZip2Constants.java
+++ src/main/java/org/apache/commons/compress/compressors/bzip2/BZip2Constants.java
@@ -36,6 +36,13 @@ interface BZip2Constants {
     int MAX_SELECTORS = (2 + (900000 / G_SIZE));
     int NUM_OVERSHOOT_BYTES = 20;
 
+    /**
+     * This array really shouldn't be here.
+     * Again, for historical purposes it is.
+     *
+     * <p>FIXME: This array should be in a private or package private
+     * location, since it could be modified by malicious code.</p>
+     */
     int[] rNums = {
         619, 720, 127, 481, 931, 816, 813, 233, 566, 247,
         985, 724, 205, 454, 863, 491, 741, 242, 949, 214,
