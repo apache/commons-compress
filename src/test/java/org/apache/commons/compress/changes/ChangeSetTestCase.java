814243d1dd81f150f918e787d8ab8535ada7909b
Torsten Curdt <tcurdt@apache.org>
2009-01-09 10:13:11 +0000

    applied a modified patch from Christian Grobmeier
    add and delete support
    https://issues.apache.org/jira/browse/SANDBOX-183
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/compress/trunk@732999 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/changes/ChangeSetTestCase.java
+++ src/test/java/org/apache/commons/compress/changes/ChangeSetTestCase.java
@@ -18,38 +18,226 @@
  */
 package org.apache.commons.compress.changes;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import junit.framework.TestCase;
 
-import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.memory.MemoryArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.ar.ArArchiveEntry;
+import org.apache.commons.compress.archivers.jar.JarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 
 public final class ChangeSetTestCase extends TestCase {
 
-	private void apply( final ChangeSet cs ) throws IOException {
-		
-		final ArchiveInputStream is = new MemoryArchiveInputStream(new String[][] {
-				{ "test1",      "" },
-				{ "test2",      "" },
-				{ "dir1/test1", "" },
-				{ "dir1/test2", "" },
-				{ "dir2/test1", "" },
-				{ "dir2/test2", "" }
-				});
-		
-		while(true) {
-			final ArchiveEntry entry = is.getNextEntry();
+	public void testDeleteFromZip() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
 			
-			if (entry == null) {
-				break;
-			}
+			final File input = new File(getClass().getClassLoader().getResource("bla.zip").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("zip", is);
 			
-			// delete, new name, new content
+			File temp = File.createTempFile("test", ".zip");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("zip", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
 		}
 	}
 	
-	public void testChangeSet() {	    
+	public void testDeleteFromTar() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.tar").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("tar", is);
+			
+			File temp = File.createTempFile("test", ".tar");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("tar", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
+	}
+
+	public void testDeleteFromJar() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			changes.delete("META-INF/MANIFEST.MF");
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.jar").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("jar", is);
+			
+			File temp = File.createTempFile("test", ".jar");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("jar", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
+	}
+	
+	public void testDeleteFromAr() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.ar").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("ar", is);
+			
+			File temp = File.createTempFile("test", ".ar");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("ar", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
+	}
+	
+	public void testDeleteFromAndAddToZip() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			
+			
+			final File file1 = new File(getClass().getClassLoader().getResource("test.txt").getFile());
+			ZipArchiveEntry entry = new ZipArchiveEntry("testdata/test.txt");
+	        changes.add(entry, new FileInputStream(file1));
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.zip").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("zip", is);
+			
+			File temp = File.createTempFile("test", ".zip");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("zip", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
+	}
+
+	public void testDeleteFromAndAddToTar() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			
+			
+			final File file1 = new File(getClass().getClassLoader().getResource("test.txt").getFile());
+			
+			final TarArchiveEntry entry = new TarArchiveEntry("testdata/test.txt");
+		    entry.setModTime(0);
+		    entry.setSize(file1.length());
+		    entry.setUserId(0);
+		    entry.setGroupId(0);
+		    entry.setUserName("avalon");
+		    entry.setGroupName("excalibur");
+		    entry.setMode(0100000);
+			
+	        changes.add(entry, new FileInputStream(file1));
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.tar").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("tar", is);
+			
+			File temp = File.createTempFile("test", ".tar");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("tar", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
+	}
+	
+	public void testDeleteFromAndAddToJar() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			
+			final File file1 = new File(getClass().getClassLoader().getResource("test.txt").getFile());
+			JarArchiveEntry entry = new JarArchiveEntry("testdata/test.txt");
+	        changes.add(entry, new FileInputStream(file1));
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.jar").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("jar", is);
+			
+			File temp = File.createTempFile("test", ".jar");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("jar", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
+	}
+	
+	public void testDeleteFromAndAddToAr() throws Exception {
+		ArchiveOutputStream out = null;
+		ArchiveInputStream ais = null;
+		try {
+			ChangeSet changes = new ChangeSet();
+			changes.delete("test2.xml");
+			
+			final File file1 = new File(getClass().getClassLoader().getResource("test.txt").getFile());
+			
+			final ArArchiveEntry entry = new ArArchiveEntry("test.txt", file1.length());
+		   
+	        changes.add(entry, new FileInputStream(file1));
+			
+			final File input = new File(getClass().getClassLoader().getResource("bla.ar").getFile());
+			final InputStream is = new FileInputStream(input);
+			ais = new ArchiveStreamFactory().createArchiveInputStream("ar", is);
+			
+			File temp = File.createTempFile("test", ".ar");
+			out = new ArchiveStreamFactory().createArchiveOutputStream("ar", new FileOutputStream(temp));
+			
+			System.out.println(temp.getAbsolutePath());
+			changes.perform(ais, out);
+		} finally {
+			if(out != null) out.close();
+			if(ais != null) ais.close();
+		}
 	}
 }
