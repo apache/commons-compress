c3a330520888c81eb87482b1e15c6cb6651b620d
Stefan Bodewig <bodewig@apache.org>
2009-02-16 14:21:12 +0000

    fix whitespace
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/compress/trunk@744923 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/DetectArchiverTestCase.java
+++ src/test/java/org/apache/commons/compress/DetectArchiverTestCase.java
@@ -33,44 +33,44 @@ import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 
 public final class DetectArchiverTestCase extends AbstractTestCase {
-	public void testDetection() throws Exception {
-		final ArchiveStreamFactory factory = new ArchiveStreamFactory();
+    public void testDetection() throws Exception {
+        final ArchiveStreamFactory factory = new ArchiveStreamFactory();
 
-		final ArchiveInputStream ar = factory.createArchiveInputStream(
-				new BufferedInputStream(new FileInputStream(
-						new File(getClass().getClassLoader().getResource("bla.ar").getFile())))); 
-		assertNotNull(ar);
-		assertTrue(ar instanceof ArArchiveInputStream);
+        final ArchiveInputStream ar = factory.createArchiveInputStream(
+                                                                       new BufferedInputStream(new FileInputStream(
+                                                                                                                   new File(getClass().getClassLoader().getResource("bla.ar").getFile())))); 
+        assertNotNull(ar);
+        assertTrue(ar instanceof ArArchiveInputStream);
 
-		final ArchiveInputStream tar = factory.createArchiveInputStream(
-				new BufferedInputStream(new FileInputStream(
-						new File(getClass().getClassLoader().getResource("bla.tar").getFile()))));
-		assertNotNull(tar);
-		assertTrue(tar instanceof TarArchiveInputStream);
+        final ArchiveInputStream tar = factory.createArchiveInputStream(
+                                                                        new BufferedInputStream(new FileInputStream(
+                                                                                                                    new File(getClass().getClassLoader().getResource("bla.tar").getFile()))));
+        assertNotNull(tar);
+        assertTrue(tar instanceof TarArchiveInputStream);
 
-		final ArchiveInputStream zip = factory.createArchiveInputStream(
-				new BufferedInputStream(new FileInputStream(
-						new File(getClass().getClassLoader().getResource("bla.zip").getFile()))));
-		assertNotNull(zip);
-		assertTrue(zip instanceof ZipArchiveInputStream);
+        final ArchiveInputStream zip = factory.createArchiveInputStream(
+                                                                        new BufferedInputStream(new FileInputStream(
+                                                                                                                    new File(getClass().getClassLoader().getResource("bla.zip").getFile()))));
+        assertNotNull(zip);
+        assertTrue(zip instanceof ZipArchiveInputStream);
 
-		final ArchiveInputStream jar = factory.createArchiveInputStream(
-				new BufferedInputStream(new FileInputStream(
-						new File(getClass().getClassLoader().getResource("bla.jar").getFile()))));
-		assertNotNull(jar);
-		assertTrue(jar instanceof JarArchiveInputStream);
+        final ArchiveInputStream jar = factory.createArchiveInputStream(
+                                                                        new BufferedInputStream(new FileInputStream(
+                                                                                                                    new File(getClass().getClassLoader().getResource("bla.jar").getFile()))));
+        assertNotNull(jar);
+        assertTrue(jar instanceof JarArchiveInputStream);
 
-		final ArchiveInputStream cpio = factory.createArchiveInputStream(
-				new BufferedInputStream(new FileInputStream(
-						new File(getClass().getClassLoader().getResource("bla.cpio").getFile()))));
-		assertNotNull(cpio);
-		assertTrue(cpio instanceof CpioArchiveInputStream);
+        final ArchiveInputStream cpio = factory.createArchiveInputStream(
+                                                                         new BufferedInputStream(new FileInputStream(
+                                                                                                                     new File(getClass().getClassLoader().getResource("bla.cpio").getFile()))));
+        assertNotNull(cpio);
+        assertTrue(cpio instanceof CpioArchiveInputStream);
 
-//		final ArchiveInputStream tgz = factory.createArchiveInputStream(
-//				new BufferedInputStream(new FileInputStream(
-//						new File(getClass().getClassLoader().getResource("bla.tgz").getFile()))));
-//		assertTrue(tgz instanceof TarArchiveInputStream);
-		
-	}
+        //              final ArchiveInputStream tgz = factory.createArchiveInputStream(
+        //                              new BufferedInputStream(new FileInputStream(
+        //                                              new File(getClass().getClassLoader().getResource("bla.tgz").getFile()))));
+        //              assertTrue(tgz instanceof TarArchiveInputStream);
+
+    }
 
 }
