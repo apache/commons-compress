0945e15a3c8279249878322d3565e9cd16832b4a
Sebastian Bazley <sebb@apache.org>
2009-03-30 17:17:58 +0000

    Initial Javadoc Implement common write(byte) method
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@760053 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/ArchiveOutputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/ArchiveOutputStream.java
@@ -21,9 +21,46 @@ package org.apache.commons.compress.archivers;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * Archive output stream implementations are expected to override the
+ * {@link #write(byte[], int, int)} method to improve performance.
+ * They should also override {@link #close()} to ensure that any necessary
+ * trailers are added.
+ * <p>
+ * Example usage:<br/>
+ * TBA
+ */
 public abstract class ArchiveOutputStream extends OutputStream {
+    
+    /** Temporary buffer used for the {@link #write(int)} method */
+    private final byte[] oneByte = new byte[1];
+    static final int BYTE_MASK = 0xFF;
 
     public abstract void putArchiveEntry(ArchiveEntry entry) throws IOException;
 
+    /**
+     * Closes the archive entry, writing any trailer information that may
+     * be required.
+     * @throws IOException
+     */
     public abstract void closeArchiveEntry() throws IOException;
+
+    // Generic implementations of OutputStream methods that may be useful to sub-classes
+    
+    /**
+     * Writes a byte to the current archive entry.
+     *
+     * This method simply calls write( byte[], 0, 1 ).
+     *
+     * MUST be overridden if the {@link #write(byte[], int, int)} method
+     * is not overridden; may be overridden otherwise.
+     * 
+     * @param b The byte to be written.
+     * @throws IOException on error
+     */
+    public void write(int b) throws IOException {
+        oneByte[0] = (byte) (b & BYTE_MASK);
+        write(oneByte, 0, 1);
+    }
+
 }
