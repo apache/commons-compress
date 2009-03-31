9acc8f50e328eec504591ff5bb5b225736c6461e
Sebastian Bazley <sebb@apache.org>
2009-03-31 10:34:52 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@760389 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/ArchiveEntry.java
+++ src/main/java/org/apache/commons/compress/archivers/ArchiveEntry.java
@@ -23,9 +23,15 @@ package org.apache.commons.compress.archivers;
  */
 public interface ArchiveEntry {
 
+    /** The name of the entry in the archive. May refer to a file or directory or other item */
 	public String getName();
 	
+	/** The (uncompressed) size of the entry. May be -1 (SIZE_UNKNOWN) if the size is unknown */
 	public long getSize();
 	
+	/** Special value indicating that the size is unknown */
+	public static final long SIZE_UNKNOWN = -1;
+	
+	/** True if the entry refers to a directory */
 	public boolean isDirectory();
 }
