9567f87eefab9b9219080acc8cee3a2b1ec83dde
Sebastian Bazley <sebb@apache.org>
2009-04-02 23:33:43 +0000

    Add some utility methods and tests
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@761474 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/utils/ArchiveUtils.java
+++ src/main/java/org/apache/commons/compress/utils/ArchiveUtils.java
@@ -18,6 +18,8 @@
 
 package org.apache.commons.compress.utils;
 
+import java.io.UnsupportedEncodingException;
+
 import org.apache.commons.compress.archivers.ArchiveEntry;
 
 /**
@@ -47,4 +49,143 @@ public class ArchiveUtils {
         sb.append(' ').append(entry.getName());
         return sb.toString();
     }
+
+    /**
+     * Check if buffer contents matches Ascii String.
+     * 
+     * @param expected
+     * @param buffer
+     * @param offset
+     * @param length
+     * @return <code>true</code> if buffer is the same as the expected string
+     */
+    public static boolean matchAsciiBuffer(
+            String expected, byte[] buffer, int offset, int length){
+        byte[] buffer1;
+        try {
+            buffer1 = expected.getBytes("ASCII");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e); // Should not happen
+        }
+        return isEqual(buffer1, 0, buffer1.length, buffer, offset, length, false);
+    }
+    
+    /**
+     * Check if buffer contents matches Ascii String.
+     * 
+     * @param expected
+     * @param buffer
+     * @return <code>true</code> if buffer is the same as the expected string
+     */
+    public static boolean matchAsciiBuffer(String expected, byte[] buffer){
+        byte[] buffer1;
+        try {
+            buffer1 = expected.getBytes("ASCII");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e); // Should not happen
+        }
+        return isEqual(buffer1, 0, buffer1.length, buffer, 0, buffer.length, false);
+    }
+    
+    /**
+     * Compare byte buffers, optionally ignoring trailing nulls
+     * 
+     * @param buffer1
+     * @param offset1
+     * @param length1
+     * @param buffer2
+     * @param offset2
+     * @param length2
+     * @param ignoreTrailingNulls
+     * @return <code>true</code> if buffer1 and buffer2 have same contents, having regard to trailing nulls
+     */
+    public static boolean isEqual(
+            final byte[] buffer1, final int offset1, final int length1,
+            final byte[] buffer2, final int offset2, final int length2,
+            boolean ignoreTrailingNulls){
+        int minLen=length1 < length2 ? length1 : length2;
+        for (int i=0; i < minLen; i++){
+            if (buffer1[offset1+i] != buffer2[offset2+i]){
+                return false;
+            }
+        }
+        if (length1 == length2){
+            return true;
+        }
+        if (ignoreTrailingNulls){
+            if (length1 > length2){
+                for(int i = length2; i < length1; i++){
+                    if (buffer1[offset1+i] != 0){
+                        return false;
+                    }
+                }
+            } else {
+                for(int i = length1; i < length2; i++){
+                    if (buffer2[offset2+i] != 0){
+                        return false;
+                    }
+                }                
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Compare byte buffers
+     * 
+     * @param buffer1
+     * @param offset1
+     * @param length1
+     * @param buffer2
+     * @param offset2
+     * @param length2
+     * @return <code>true</code> if buffer1 and buffer2 have same contents
+     */
+    public static boolean isEqual(
+            final byte[] buffer1, final int offset1, final int length1,
+            final byte[] buffer2, final int offset2, final int length2){
+        return isEqual(buffer1, offset1, length1, buffer2, offset2, length2, false);
+    }
+    
+    /**
+     * Compare byte buffers
+     * 
+     * @param buffer1
+     * @param buffer2
+     * @return <code>true</code> if buffer1 and buffer2 have same contents
+     */
+    public static boolean isEqual(final byte[] buffer1, final byte[] buffer2 ){
+        return isEqual(buffer1, 0, buffer1.length, buffer2, 0, buffer2.length, false);
+    }
+    
+    /**
+     * Compare byte buffers, optionally ignoring trailing nulls
+     * 
+     * @param buffer1
+     * @param buffer2
+     * @param ignoreTrailingNulls
+     * @return <code>true</code> if buffer1 and buffer2 have same contents
+     */
+    public static boolean isEqual(final byte[] buffer1, final byte[] buffer2, boolean ignoreTrailingNulls){
+        return isEqual(buffer1, 0, buffer1.length, buffer2, 0, buffer2.length, ignoreTrailingNulls);
+    }
+    
+    /**
+     * Compare byte buffers, ignoring trailing nulls
+     * 
+     * @param buffer1
+     * @param offset1
+     * @param length1
+     * @param buffer2
+     * @param offset2
+     * @param length2
+     * @return <code>true</code> if buffer1 and buffer2 have same contents, having regard to trailing nulls
+     */
+    public static boolean isEqualWithNull(
+            final byte[] buffer1, final int offset1, final int length1,
+            final byte[] buffer2, final int offset2, final int length2){
+        return isEqual(buffer1, offset1, length1, buffer2, offset2, length2, true);
+    }
+    
 }
