8e5f9c9a1c61ec96da63a1a53955b4f57e10b404
Christian Grobmeier <grobmeier@apache.org>
2009-04-19 11:28:24 +0000

    added javadocs
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@766452 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/changes/ChangeSetResults.java
+++ src/main/java/org/apache/commons/compress/changes/ChangeSetResults.java
@@ -29,39 +29,61 @@ public class ChangeSetResults {
     private List addedFromStream = new ArrayList();
     private List deleted = new ArrayList();
     
+    /**
+     * Adds the filename of a recently deleted file to the result list.
+     * @param fileName the file which has been deleted
+     */
     void deleted(String fileName) {
         deleted.add(fileName);
     }
     
+    /**
+     * Adds the name of a file to the result list which has been 
+     * copied from the source stream to the target stream.
+     * @param fileName the file name which has been added from the original stream
+     */
     void addedFromStream(String fileName) {
         addedFromStream.add(fileName);
     }
     
+    /**
+     * Adds the name of a file to the result list which has been
+     * copied from the changeset to the target stream
+     * @param fileName the name of the file
+     */
     void addedFromChangeSet(String fileName) {
         addedFromChangeSet.add(fileName);
     }
 
     /**
-     * @return the addedFromChangeSet
+     * Returns a list of filenames which has been added from the changeset
+     * @return the list of filenames
      */
     public List getAddedFromChangeSet() {
         return addedFromChangeSet;
     }
 
     /**
-     * @return the addedFromStream
+     * Returns a list of filenames which has been added from the original stream
+     * @return the list of filenames
      */
     public List getAddedFromStream() {
         return addedFromStream;
     }
 
     /**
-     * @return the deleted
+     * Returns a list of filenames which has been deleted
+     * @return the list of filenames
      */
     public List getDeleted() {
         return deleted;
     }
     
+    /**
+     * Checks if an filename already has been added to the result list
+     * @param filename the filename to check
+     * @return true, if this filename already has been added
+     */
     boolean hasBeenAdded(String filename) {
         if(addedFromChangeSet.contains(filename) || addedFromStream.contains(filename)) {
             return true;
