f382ea5150ee630db8aecc6c64e4f1cec285874b
Stefan Bodewig <bodewig@apache.org>
2009-08-01 19:32:12 +0000

    check name/directory/size and lastModified read from tar is consistent with what we write
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@799911 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/archivers/ZipTestCase.java
+++ src/test/java/org/apache/commons/compress/archivers/ZipTestCase.java
@@ -24,7 +24,6 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.compress.AbstractTestCase;
