7f570594152aa237beee2b8f50edda4e29c91c4b
Sebastian Bazley <sebb@apache.org>
2010-03-13 13:33:38 +0000

    Javadoc fixes.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@922557 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/zip/UnparseableExtraFieldData.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/UnparseableExtraFieldData.java
@@ -22,8 +22,8 @@ package org.apache.commons.compress.archivers.zip;
  * Wrapper for extra field data that doesn't conform to the recommended format of header-tag + size + data.
  *
  * <p>The header-id is artificial (and not listed as a known ID in
- * {@link http://www.pkware.com/documents/casestudies/APPNOTE.TXT
- * APPNOTE.TXT}.  Since it isn't used anywhere except to satisfy the
+ * {@link <a href="http://www.pkware.com/documents/casestudies/APPNOTE.TXT">
+ * APPNOTE.TXT</a>}).  Since it isn't used anywhere except to satisfy the
  * ZipExtraField contract it shouldn't matter anyway.</p>
  * @NotThreadSafe
  */
