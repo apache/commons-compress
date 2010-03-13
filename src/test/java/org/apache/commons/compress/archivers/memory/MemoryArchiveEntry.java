44403d6308ec3141df05d57d9b1810aa833fbdcc
Sebastian Bazley <sebb@apache.org>
2010-03-13 13:44:50 +0000

    Misspelt Javadoc tag
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@922565 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/archivers/memory/MemoryArchiveEntry.java
+++ src/test/java/org/apache/commons/compress/archivers/memory/MemoryArchiveEntry.java
@@ -43,7 +43,7 @@ public final class MemoryArchiveEntry implements ArchiveEntry {
         return false;
     }
 
-    /** {@inheritDocs} */
+    /** {@inheritDoc} */
     public Date getLastModifiedDate() {
         return new Date();
     }
