6e5a582479e545023750beb86deb3b0d92b78963
Sebastian Bazley <sebb@apache.org>
2010-05-10 14:12:34 +0000

    COMPRESS-107 - recognise Ant tar files
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@942746 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
@@ -326,24 +326,33 @@ public class TarArchiveInputStream extends ArchiveInputStream {
             return false;
         }
 
-        if (ArchiveUtils.matchAsciiBuffer(TarConstants.MAGIC_POSIX, 
+        if (ArchiveUtils.matchAsciiBuffer(TarConstants.MAGIC_POSIX,
                 signature, TarConstants.MAGIC_OFFSET, TarConstants.MAGICLEN)
             &&
-            ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_POSIX, 
+            ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_POSIX,
                 signature, TarConstants.VERSION_OFFSET, TarConstants.VERSIONLEN)
                 ){
             return true;
         }
-        if (ArchiveUtils.matchAsciiBuffer(TarConstants.MAGIC_GNU, 
+        if (ArchiveUtils.matchAsciiBuffer(TarConstants.MAGIC_GNU,
                 signature, TarConstants.MAGIC_OFFSET, TarConstants.MAGICLEN)
             &&
             (
-             ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_GNU_SPACE, 
+             ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_GNU_SPACE,
                 signature, TarConstants.VERSION_OFFSET, TarConstants.VERSIONLEN)
             ||
-            ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_GNU_ZERO, 
+            ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_GNU_ZERO,
                 signature, TarConstants.VERSION_OFFSET, TarConstants.VERSIONLEN)
             )
+                ){
+            return true;
+        }
+        // COMPRESS-107 - recognise Ant tar files
+        if (ArchiveUtils.matchAsciiBuffer(TarConstants.MAGIC_ANT,
+                signature, TarConstants.MAGIC_OFFSET, TarConstants.MAGICLEN)
+            &&
+            ArchiveUtils.matchAsciiBuffer(TarConstants.VERSION_ANT,
+                signature, TarConstants.VERSION_OFFSET, TarConstants.VERSIONLEN)
                 ){
             return true;
         }
