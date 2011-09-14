87436750cedf64c3bf25f948597f53af2b52221a
Stefan Bodewig <bodewig@apache.org>
2011-09-14 14:33:48 +0000

    Utility class that "normalizes" a JAR archive so it can be signed without Pack200 breaking the signature
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1170644 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/compress/compressors/pack200/Pack200UtilsTest.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.commons.compress.compressors.pack200;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+import org.apache.commons.compress.AbstractTestCase;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.utils.IOUtils;
+
+public final class Pack200UtilsTest extends AbstractTestCase {
+
+    public void testNormalize() throws Throwable {
+        final File input = getFile("bla.jar");
+        final File[] output = createTempDirAndFile();
+        try {
+            Pack200Utils.normalize(input, output[1],
+                                   new HashMap<String, String>());
+            final FileInputStream is = new FileInputStream(output[1]);
+            try {
+                final ArchiveInputStream in = new ArchiveStreamFactory()
+                    .createArchiveInputStream("jar", is);
+
+                ArchiveEntry entry = in.getNextEntry();
+                while (entry != null) {
+                    File archiveEntry = new File(dir, entry.getName());
+                    archiveEntry.getParentFile().mkdirs();
+                    if (entry.isDirectory()) {
+                        archiveEntry.mkdir();
+                        entry = in.getNextEntry();
+                        continue;
+                    }
+                    OutputStream out = new FileOutputStream(archiveEntry);
+                    IOUtils.copy(in, out);
+                    out.close();
+                    entry = in.getNextEntry();
+                }
+
+                in.close();
+            } finally {
+                is.close();
+            }
+        } finally {
+            output[1].delete();
+            output[0].delete();
+        }
+    }
+
+}
