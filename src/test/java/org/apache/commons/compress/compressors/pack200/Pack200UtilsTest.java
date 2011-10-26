5eab47809d585ca5746d3abc24ee23507f28dd25
Gary D. Gregory <ggregory@apache.org>
2011-10-26 13:24:59 +0000

    Remove unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1189197 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/compressors/pack200/Pack200UtilsTest.java
+++ src/test/java/org/apache/commons/compress/compressors/pack200/Pack200UtilsTest.java
@@ -21,7 +21,6 @@ package org.apache.commons.compress.compressors.pack200;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 
