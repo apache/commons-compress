f51fee1afa05a209d60665ef772504b4f788cad4
Stefan Bodewig <bodewig@apache.org>
2011-10-27 08:51:20 +0000

    Improve test-coverage for DumpArchiveEntry and fix a bug found by doing so
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1189680 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/compress/archivers/dump/DumpArchiveEntryTest.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.commons.compress.archivers.dump;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DumpArchiveEntryTest {
+    @Test
+    public void publicNameAddsTrailingSlashForDirectories() {
+        DumpArchiveEntry ent = new DumpArchiveEntry("foo", "bar", -1,
+                                                    DumpArchiveEntry.TYPE
+                                                    .DIRECTORY);
+        assertEquals("bar", ent.getSimpleName());
+        assertEquals("foo", ent.getOriginalName());
+        assertEquals("foo/", ent.getName());
+    }
+
+    @Test
+    public void publicNameRemovesLeadingDotSlash() {
+        DumpArchiveEntry ent = new DumpArchiveEntry("./foo", "bar");
+        assertEquals("bar", ent.getSimpleName());
+        assertEquals("./foo", ent.getOriginalName());
+        assertEquals("foo", ent.getName());
+    }
+
+}
