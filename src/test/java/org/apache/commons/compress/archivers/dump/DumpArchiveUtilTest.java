f3b51a5272540f55a62012897ec50da8b5356c81
Stefan Bodewig <bodewig@apache.org>
2011-10-27 08:22:48 +0000

    remove unneeded cast to int in bitwise operations, add tests for convert methods
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1189662 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/compress/archivers/dump/DumpArchiveUtilTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.commons.compress.archivers.dump;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DumpArchiveUtilTest {
+
+    @Test
+    public void convert64() {
+        assertEquals(0xABCDEF0123456780L,
+                     DumpArchiveUtil.convert64(new byte[] {
+                             (byte) 0x80, 0x67, 0x45, 0x23, 1, (byte) 0xEF,
+                             (byte) 0xCD, (byte) 0xAB
+                         }, 0));
+    }
+
+    @Test
+    public void convert32() {
+        assertEquals(0xABCDEF01,
+                     DumpArchiveUtil.convert32(new byte[] {
+                             1, (byte) 0xEF, (byte) 0xCD, (byte) 0xAB
+                         }, 0));
+    }
+
+    @Test
+    public void convert16() {
+        assertEquals(0xABCD,
+                     DumpArchiveUtil.convert16(new byte[] {
+                             (byte) 0xCD, (byte) 0xAB
+                         }, 0));
+    }
+}
