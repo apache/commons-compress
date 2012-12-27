f5f7afdcfb0f7a73d4f8148e09402a2b5d59d971
Stefan Bodewig <bodewig@apache.org>
2012-12-27 21:48:50 +0000

    COMPRESS-203 actually, the file name must be fixed after it has been truncated
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1426338 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveOutputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveOutputStream.java
@@ -454,13 +454,13 @@ public class TarArchiveOutputStream extends ArchiveOutputStream {
     void writePaxHeaders(String entryName,
                          Map<String, String> headers) throws IOException {
         String name = "./PaxHeaders.X/" + stripTo7Bits(entryName);
+        if (name.length() >= TarConstants.NAMELEN) {
+            name = name.substring(0, TarConstants.NAMELEN - 1);
+        }
         while (name.endsWith("/")) {
             // TarEntry's constructor would think this is a directory
             // and not allow any data to be written
             name = name.substring(0, name.length() - 1);
-        }
-        if (name.length() >= TarConstants.NAMELEN) {
-            name = name.substring(0, TarConstants.NAMELEN - 1);
         }
         TarArchiveEntry pex = new TarArchiveEntry(name,
                                                   TarConstants.LF_PAX_EXTENDED_HEADER_LC);
