e2cf412a7856a7a9f314c3927df2f5a04f42177d
Stefan Bodewig <bodewig@apache.org>
2012-12-28 06:53:21 +0000

    COMPRESS-189 Inflater may return 0 if it needs more input, we don't handle this case properly
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1426417 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/archivers/zip/ZipArchiveInputStreamTest.java
+++ src/test/java/org/apache/commons/compress/archivers/zip/ZipArchiveInputStreamTest.java
@@ -19,12 +19,15 @@
 package org.apache.commons.compress.archivers.zip;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URI;
 import java.net.URL;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ZipArchiveInputStreamTest {
@@ -43,6 +46,35 @@ public class ZipArchiveInputStreamTest {
             zae = in.getNextZipEntry();
             assertEquals("\u00e4/", zae.getName());
         } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    /**
+     * @see "https://issues.apache.org/jira/browse/COMPRESS-189"
+     */
+    @Test
+    @Ignore
+    public void properUseOfInflater() throws Exception {
+        URL zip = getClass().getResource("/COMPRESS-189.zip");
+        ZipFile zf = null;
+        ZipArchiveInputStream in = null;
+        try {
+            zf = new ZipFile(new File(new URI(zip.toString())));
+            ZipArchiveEntry zae = zf.getEntry("USD0558682-20080101.ZIP");
+            in = new ZipArchiveInputStream(new BufferedInputStream(zf.getInputStream(zae)));
+            ZipArchiveEntry innerEntry;
+            while ((innerEntry = in.getNextZipEntry()) != null) {
+                if (innerEntry.getName().endsWith("XML")) {
+                    assertTrue(0 < in.read());
+                }
+            }
+        } finally {
+            if (zf != null) {
+                zf.close();
+            }
             if (in != null) {
                 in.close();
             }
