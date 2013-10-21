266f038dc822c4b05ca183cf9052aaddaa1cdcc8
Stefan Bodewig <bodewig@apache.org>
2013-10-21 15:46:00 +0000

    defer creation of output stream until data is written in order to keep empty streams empty - LZMA2 writes dictionary information, for example
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1534234 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/compress/archivers/sevenz/SevenZOutputFileTest.java
@@ -0,0 +1,124 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.commons.compress.archivers.sevenz;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import org.apache.commons.compress.AbstractTestCase;
+
+public class SevenZOutputFileTest extends AbstractTestCase {
+
+    private File output;
+
+    public void tearDown() throws Exception {
+        if (output != null && !output.delete()) {
+            output.deleteOnExit();
+        }
+    }
+
+    public void testDirectoriesAndEmptyFiles() throws Exception {
+        File output = new File(dir, "empties.7z");
+
+        Date accessDate = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -1);
+        Date creationDate = cal.getTime();
+
+        SevenZOutputFile outArchive = new SevenZOutputFile(output);
+        try {
+            SevenZArchiveEntry entry = outArchive.createArchiveEntry(dir, "foo/");
+            outArchive.putArchiveEntry(entry);
+            outArchive.closeArchiveEntry();
+
+            entry = new SevenZArchiveEntry();
+            entry.setName("foo/bar");
+            entry.setCreationDate(creationDate);
+            entry.setAccessDate(accessDate);
+            outArchive.putArchiveEntry(entry);
+            outArchive.write(new byte[0]);
+            outArchive.closeArchiveEntry();
+
+            entry = new SevenZArchiveEntry();
+            entry.setName("xyzzy");
+            outArchive.putArchiveEntry(entry);
+            outArchive.write(0);
+            outArchive.closeArchiveEntry();
+
+            entry = outArchive.createArchiveEntry(dir, "baz/");
+            entry.setAntiItem(true);
+            outArchive.putArchiveEntry(entry);
+            outArchive.closeArchiveEntry();
+
+            entry = new SevenZArchiveEntry();
+            entry.setName("dada");
+            outArchive.putArchiveEntry(entry);
+            outArchive.write(5);
+            outArchive.closeArchiveEntry();
+
+            outArchive.finish();
+        } finally {
+            outArchive.close();
+        }
+
+        final SevenZFile archive = new SevenZFile(output);
+        try {
+            SevenZArchiveEntry entry = archive.getNextEntry();
+            assert(entry != null);
+            assertEquals("foo/", entry.getName());
+            assertTrue(entry.isDirectory());
+            assertFalse(entry.isAntiItem());
+
+            entry = archive.getNextEntry();
+            assert(entry != null);
+            assertEquals("foo/bar", entry.getName());
+            assertFalse(entry.isDirectory());
+            assertFalse(entry.isAntiItem());
+            assertEquals(0, entry.getSize());
+            assertFalse(entry.getHasLastModifiedDate());
+            assertEquals(accessDate, entry.getAccessDate());
+            assertEquals(creationDate, entry.getCreationDate());
+
+            entry = archive.getNextEntry();
+            assert(entry != null);
+            assertEquals("xyzzy", entry.getName());
+            assertEquals(1, entry.getSize());
+            assertFalse(entry.getHasAccessDate());
+            assertFalse(entry.getHasCreationDate());
+            assertEquals(0, archive.read());
+
+            entry = archive.getNextEntry();
+            assert(entry != null);
+            assertEquals("baz/", entry.getName());
+            assertTrue(entry.isDirectory());
+            assertTrue(entry.isAntiItem());
+
+            entry = archive.getNextEntry();
+            assert(entry != null);
+            assertEquals("dada", entry.getName());
+            assertEquals(1, entry.getSize());
+            assertEquals(5, archive.read());
+
+            assert(archive.getNextEntry() == null);
+        } finally {
+            archive.close();
+        }
+
+    }
+
+}
