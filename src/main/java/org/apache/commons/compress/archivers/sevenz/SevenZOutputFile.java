ebb81fca71c7330e51794409942e3ea3d06e7412
Stefan Bodewig <bodewig@apache.org>
2013-12-20 12:13:11 +0000

    COMPRESS-252 off-by-one error writing bitsets that leads to corrupt 7z headers
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1552608 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/sevenz/SevenZOutputFile.java
+++ src/main/java/org/apache/commons/compress/archivers/sevenz/SevenZOutputFile.java
@@ -636,14 +636,13 @@ public class SevenZOutputFile {
         int shift = 7;
         for (int i = 0; i < length; i++) {
             cache |= ((bits.get(i) ? 1 : 0) << shift);
-            --shift;
-            if (shift == 0) {
+            if (--shift < 0) {
                 header.write(cache);
                 shift = 7;
                 cache = 0;
             }
         }
-        if (length > 0 && shift > 0) {
+        if (shift != 7) {
             header.write(cache);
         }
     }
