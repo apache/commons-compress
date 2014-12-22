e214adb4b9164910d62614dbd7b3043cb0cc3467
Kristian Rosenvold <krosenvold@apache.org>
2014-12-22 15:24:02 +0000

    COMPRESS-296 Parallel compression. Added StreamCompressor and ScatterZipOutputStream.
    StreamCompressor is an extract of the deflation algorithm from ZipArchiveOutputStream, which unfortunately
    was too conflated with writing a file in a particular structure. Using the actual zip file format as an
    intermediate format for scatter-streams turned out to be fairly inefficient. ScatterZipOuputStream
    is 2-3x faster than using a zip file as intermediate format.
    
    It would be possibly to refactor ZipArchiveOutputStream to use StreamCompressor, but there would
    be a slight break in backward compatibility regarding the protected writeOut method, which
    is moved to the streamCompressor class.
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1647329 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/compress/archivers/zip/ScatterZipOutputStreamTest.java
@@ -0,0 +1,58 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.commons.compress.archivers.zip;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class ScatterZipOutputStreamTest {
+
+    @Test
+    public void putArchiveEntry() throws Exception {
+        File scatteFile = File.createTempFile("scattertest", ".notzip");
+        ScatterZipOutputStream scatterZipOutputStream = ScatterZipOutputStream.fileBased(scatteFile);
+        final byte[] B_PAYLOAD = "RBBBBBBS".getBytes();
+        final byte[] A_PAYLOAD = "XAAY".getBytes();
+
+        ZipArchiveEntry zab = new ZipArchiveEntry("b.txt");
+        scatterZipOutputStream.addArchiveEntry(zab, new ByteArrayInputStream(B_PAYLOAD), ZipArchiveEntry.DEFLATED);
+
+        ZipArchiveEntry zae = new ZipArchiveEntry("a.txt");
+        scatterZipOutputStream.addArchiveEntry(zae, new ByteArrayInputStream(A_PAYLOAD), ZipArchiveEntry.DEFLATED);
+
+        File target = File.createTempFile("scattertest", ".zip");
+        ZipArchiveOutputStream outputStream = new ZipArchiveOutputStream(target);
+        scatterZipOutputStream.writeTo( outputStream);
+        outputStream.close();
+
+        ZipFile zf = new ZipFile(target);
+        final ZipArchiveEntry b_entry = zf.getEntries("b.txt").iterator().next();
+        assertEquals(8, b_entry.getSize());
+        assertArrayEquals(B_PAYLOAD, IOUtils.toByteArray(zf.getInputStream(b_entry)));
+
+        final ZipArchiveEntry a_entry = zf.getEntries("a.txt").iterator().next();
+        assertEquals(4, a_entry.getSize());
+        assertArrayEquals(A_PAYLOAD, IOUtils.toByteArray(zf.getInputStream(a_entry)));
+    }
+}
