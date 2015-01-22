0911c6ec7ead06cf9594847412b24bd56e98cc46
Stefan Bodewig <bodewig@apache.org>
2015-01-22 16:32:24 +0000

    unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1653937 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/compress/archivers/zip/ParallelScatterZipCreator.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/ParallelScatterZipCreator.java
@@ -19,7 +19,6 @@ package org.apache.commons.compress.archivers.zip;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
