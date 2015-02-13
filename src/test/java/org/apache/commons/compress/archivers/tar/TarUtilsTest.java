dc4c6ebd740cee8b245f7499ed33be2f3a8b7961
Stefan Bodewig <bodewig@apache.org>
2015-02-13 19:12:09 +0000

    python tarfile library seems to create embedded NULs, COMPRESS-301, unit test to follow
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1659649 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/archivers/tar/TarUtilsTest.java
+++ src/test/java/org/apache/commons/compress/archivers/tar/TarUtilsTest.java
@@ -95,12 +95,6 @@ public class TarUtilsTest extends TestCase {
             fail("Expected IllegalArgumentException - embedded space");
         } catch (IllegalArgumentException expected) {
         }
-        buffer = " 0\00007 ".getBytes(CharsetNames.UTF_8); // Invalid - embedded NUL
-        try {
-            TarUtils.parseOctal(buffer,0, buffer.length);
-            fail("Expected IllegalArgumentException - embedded NUL");
-        } catch (IllegalArgumentException expected) {
-        }
     }
 
     private void checkRoundTripOctal(final long value, final int bufsize) {
