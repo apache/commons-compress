5cf68bdc46c7bcc0fc5eb3cacd1c84514ef552cf
Stefan Bodewig <bodewig@apache.org>
2015-02-20 16:18:00 +0000

    return 0 from read when asked to read 0 bytes - COMPRESS-309
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1661151 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/compressors/bzip2/BZip2CompressorInputStreamTest.java
+++ src/test/java/org/apache/commons/compress/compressors/bzip2/BZip2CompressorInputStreamTest.java
@@ -20,8 +20,11 @@ package org.apache.commons.compress.compressors.bzip2;
 
 import static org.apache.commons.compress.AbstractTestCase.getFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BZip2CompressorInputStreamTest {
@@ -34,6 +37,36 @@ public class BZip2CompressorInputStreamTest {
         } finally {
             in.close();
         }
+    }
+
+    /**
+     * @see "https://issues.apache.org/jira/browse/COMPRESS-309"
+     */
+    @Test
+    public void readOfLength0ShouldReturn0() throws Exception {
+        // Create a big random piece of data
+        byte[] rawData = new byte[1048576];
+        for (int i=0; i < rawData.length; ++i) {
+            rawData[i] = (byte) Math.floor(Math.random()*256);
+        }
+
+        // Compress it
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BZip2CompressorOutputStream bzipOut = new BZip2CompressorOutputStream(baos);
+        bzipOut.write(rawData);
+        bzipOut.flush();
+        bzipOut.close();
+        baos.flush();
+        baos.close();
+
+        // Try to read it back in
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        BZip2CompressorInputStream bzipIn = new BZip2CompressorInputStream(bais);
+        byte[] buffer = new byte[1024];
+        Assert.assertEquals(1024, bzipIn.read(buffer, 0, 1024));
+        Assert.assertEquals(0, bzipIn.read(buffer, 1024, 0));
+        Assert.assertEquals(1024, bzipIn.read(buffer, 0, 1024));
+        bzipIn.close();
     }
 
 }
