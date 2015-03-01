c12c48ce91c6b5cceb61043a914d0b438b7a6900
Sebastian Bazley <sebb@apache.org>
2015-03-01 13:36:51 +0000

    Name the parameterised tests
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/compress/trunk@1663097 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/compress/archivers/LongPathTest.java
+++ src/test/java/org/apache/commons/compress/archivers/LongPathTest.java
@@ -60,8 +60,8 @@ public class LongPathTest extends AbstractTestCase {
 
     private File file;
 
-    public LongPathTest(File file){
-        this.file = file;
+    public LongPathTest(String file){
+        this.file = new File(ARCDIR, file);
     }
 
     @BeforeClass
@@ -79,10 +79,10 @@ public class LongPathTest extends AbstractTestCase {
         br.close();
     }
 
-    @Parameters
+    @Parameters(name = "file={0}")
     public static Collection<Object[]> data() {
         Collection<Object[]> params = new ArrayList<Object[]>();
-        for (File f : ARCDIR.listFiles(new FilenameFilter() {
+        for (String f : ARCDIR.list(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return !name.endsWith(".txt");
             }
