a5eca568368b287f875149133a7ff6029b07aad8
Stefan Bodewig <bodewig@apache.org>
2016-03-22 18:14:07 +0100

    COMPRESS-344 accept NUL as terminator for GNU extended names

--- src/main/java/org/apache/commons/compress/archivers/ar/ArArchiveInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/ar/ArArchiveInputStream.java
@@ -177,7 +177,7 @@ public class ArArchiveInputStream extends ArchiveInputStream {
             throw new IOException("Cannot process GNU long filename as no // record was found");
         }
         for(int i=offset; i < namebuffer.length; i++){
-            if (namebuffer[i]=='\012'){
+            if (namebuffer[i] == '\012' || namebuffer[i] == 0){
                 if (namebuffer[i-1]=='/') {
                     i--; // drop trailing /
                 }
