e71827b5db22d81e0b009f3e205173ce643d2e8e
Gary Gregory <ggregory@apache.org>
2016-09-05 14:41:24 -0400

    Use try with resources.

--- src/test/java/org/apache/commons/compress/utils/BitInputStreamTest.java
+++ src/test/java/org/apache/commons/compress/utils/BitInputStreamTest.java
@@ -30,92 +30,92 @@ public class BitInputStreamTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAllowReadingOfANegativeAmountOfBits() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        bis.readBits(-1);
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            bis.readBits(-1);
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAllowReadingOfMoreThan63BitsAtATime() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        bis.readBits(64);
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            bis.readBits(64);
+        }
     }
 
     @Test
     public void testReading24BitsInLittleEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        assertEquals(0x000140f8, bis.readBits(24));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(0x000140f8, bis.readBits(24));
+        }
     }
 
     @Test
     public void testReading24BitsInBigEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN);
-        assertEquals(0x00f84001, bis.readBits(24));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN)) {
+            assertEquals(0x00f84001, bis.readBits(24));
+        }
     }
 
     @Test
     public void testReading17BitsInLittleEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        assertEquals(0x000140f8, bis.readBits(17));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(0x000140f8, bis.readBits(17));
+        }
     }
 
     @Test
     public void testReading17BitsInBigEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN);
-        // 1-11110000-10000000
-        assertEquals(0x0001f080, bis.readBits(17));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN)) {
+            // 1-11110000-10000000
+            assertEquals(0x0001f080, bis.readBits(17));
+        }
     }
 
     @Test
     public void testReading30BitsInLittleEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        assertEquals(0x2f0140f8, bis.readBits(30));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(0x2f0140f8, bis.readBits(30));
+        }
     }
 
     @Test
     public void testReading30BitsInBigEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN);
-        // 111110-00010000-00000000-01001011
-        assertEquals(0x3e10004b, bis.readBits(30));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN)) {
+            // 111110-00010000-00000000-01001011
+            assertEquals(0x3e10004b, bis.readBits(30));
+        }
     }
 
     @Test
     public void testReading31BitsInLittleEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        assertEquals(0x2f0140f8, bis.readBits(31));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(0x2f0140f8, bis.readBits(31));
+        }
     }
 
     @Test
     public void testReading31BitsInBigEndian() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN);
-        // 1111100-00100000-00000000-10010111
-        assertEquals(0x7c200097, bis.readBits(31));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.BIG_ENDIAN)) {
+            // 1111100-00100000-00000000-10010111
+            assertEquals(0x7c200097, bis.readBits(31));
+        }
     }
 
     @Test
     public void testClearBitCache() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        assertEquals(0x08, bis.readBits(4));
-        bis.clearBitCache();
-        assertEquals(0, bis.readBits(1));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(0x08, bis.readBits(4));
+            bis.clearBitCache();
+            assertEquals(0, bis.readBits(1));
+        }
     }
 
     @Test
     public void testEOF() throws IOException {
-        final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN);
-        assertEquals(0x2f0140f8, bis.readBits(30));
-        assertEquals(-1, bis.readBits(3));
-        bis.close();
+        try (final BitInputStream bis = new BitInputStream(getStream(), ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(0x2f0140f8, bis.readBits(30));
+            assertEquals(-1, bis.readBits(3));
+        }
     }
 
     /**
@@ -135,15 +135,15 @@ public class BitInputStreamTest {
                 33, // 00100001
                 74  // 01001010
             });
-        BitInputStream bin = new BitInputStream(in, ByteOrder.LITTLE_ENDIAN);
-        assertEquals(23, // 10111
-                     bin.readBits(5));
-        assertEquals(714595605644185962l, // 0001-00111101-01011000-00011101-01011010-00001111-01000010-00101101-010
-                     bin.readBits(63));
-        assertEquals(1186, // 01001010-0010
-                     bin.readBits(12));
-        assertEquals(-1 , bin.readBits(1));
-        bin.close();
+        try (final BitInputStream bin = new BitInputStream(in, ByteOrder.LITTLE_ENDIAN)) {
+            assertEquals(23, // 10111
+                         bin.readBits(5));
+            assertEquals(714595605644185962l, // 0001-00111101-01011000-00011101-01011010-00001111-01000010-00101101-010
+                         bin.readBits(63));
+            assertEquals(1186, // 01001010-0010
+                         bin.readBits(12));
+            assertEquals(-1 , bin.readBits(1));
+        }
     }
 
     @Test
@@ -160,15 +160,15 @@ public class BitInputStreamTest {
                 33, // 00100001
                 74  // 01001010
             });
-        BitInputStream bin = new BitInputStream(in, ByteOrder.BIG_ENDIAN);
-        assertEquals(10, // 01010
-                     bin.readBits(5));
-        assertEquals(8274274654740644818l, //111-00101101-01000010-00001111-01011010-00011101-01011000-00111101-0010
-                     bin.readBits(63));
-        assertEquals(330, // 0001-01001010
-                     bin.readBits(12));
-        assertEquals(-1 , bin.readBits(1));
-        bin.close();
+        try (final BitInputStream bin = new BitInputStream(in, ByteOrder.BIG_ENDIAN)) {
+            assertEquals(10, // 01010
+                         bin.readBits(5));
+            assertEquals(8274274654740644818l, //111-00101101-01000010-00001111-01011010-00011101-01011000-00111101-0010
+                    bin.readBits(63));
+            assertEquals(330, // 0001-01001010
+                         bin.readBits(12));
+            assertEquals(-1 , bin.readBits(1));
+        }
     }
 
     private ByteArrayInputStream getStream() {
