3fdcafde8b45306db0cffdfe3356f0a18f2eba7d
Gary Gregory <ggregory@apache.org>
2016-12-08 23:31:35 -0800

    [COMPRESS-362] Bullet-proof code using try-with-resources statements.

--- src/main/java/org/apache/commons/compress/compressors/pack200/Pack200Utils.java
+++ src/main/java/org/apache/commons/compress/compressors/pack200/Pack200Utils.java
@@ -22,7 +22,6 @@ package org.apache.commons.compress.compressors.pack200;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.jar.JarFile;
