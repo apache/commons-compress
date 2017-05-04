ccf55ddf2238983ba9f6da9ba2bb61e0c0ac573b
Stefan Bodewig <bodewig@apache.org>
2017-05-04 08:47:27 +0200

    COMPRESS-392 remove matches method if it doesn't work
    closes #23

--- src/main/java/org/apache/commons/compress/compressors/brotli/BrotliCompressorInputStream.java
+++ src/main/java/org/apache/commons/compress/compressors/brotli/BrotliCompressorInputStream.java
@@ -143,18 +143,5 @@ public class BrotliCompressorInputStream extends CompressorInputStream {
     public void reset() throws IOException {
         decIS.reset();
     }
-    
 
-    /**
-     * There is no magic for Brotli
-     * 
-     * @param signature
-     *            the bytes to check
-     * @param length
-     *            the number of bytes to check
-     * @return true
-     */
-    static boolean matches(final byte[] signature, final int length) {
-        return true;
-    }
 }
