ffe472dfd0b6b28b002ad666ecc1e26c43b8e258
Stefan Bodewig <bodewig@apache.org>
2017-06-17 21:46:53 +0200

    add an extra sanity check

--- src/main/java/org/apache/commons/compress/archivers/tar/TarUtils.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarUtils.java
@@ -519,6 +519,10 @@ public class TarUtils {
         final BigInteger val = BigInteger.valueOf(value);
         final byte[] b = val.toByteArray();
         final int len = b.length;
+        if (len > length - 1) {
+            throw new IllegalArgumentException("Value " + value +
+                " is too large for " + length + " byte field.");
+        }
         final int off = offset + length - len;
         System.arraycopy(b, 0, buf, off, len);
         final byte fill = (byte) (negative ? 0xff : 0);
