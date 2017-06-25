eb345d42b4609ace419ab840845f428427f225c6
Simon Spero <sesuncedu@gmail.com>
2017-06-25 17:57:28 -0400

    Fix after rebasing
    Signed-off-by: Simon Spero <sesuncedu@gmail.com>

--- src/test/java/org/apache/commons/compress/archivers/tar/TarArchiveOutputStreamTest.java
+++ src/test/java/org/apache/commons/compress/archivers/tar/TarArchiveOutputStreamTest.java
@@ -30,6 +30,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.security.MessageDigest;
 import java.util.Calendar;
 import java.util.Date;
