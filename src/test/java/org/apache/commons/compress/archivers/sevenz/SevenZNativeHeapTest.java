ae1f61cbd73615829b2d25a9ee8f2eb734d73119
Simon Spero <sesuncedu@gmail.com>
2017-06-30 15:16:20 -0400

    Change code and test structure to avoid jdk9 incompatible mocking
    Signed-off-by: Simon Spero <sesuncedu@gmail.com>

--- src/test/java/org/apache/commons/compress/archivers/sevenz/SevenZNativeHeapTest.java
+++ src/test/java/org/apache/commons/compress/archivers/sevenz/SevenZNativeHeapTest.java
@@ -17,48 +17,251 @@
  */
 package org.apache.commons.compress.archivers.sevenz;
 
-import org.apache.commons.compress.AbstractTestCase;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
+import org.apache.commons.compress.AbstractTestCase;
+import org.apache.commons.compress.archivers.sevenz.Coders.DeflateDecoder;
+import org.apache.commons.compress.archivers.sevenz.Coders.DeflateDecoder.DeflateDecoderInputStream;
+import org.apache.commons.compress.archivers.sevenz.Coders.DeflateDecoder.DeflateDecoderOutputStream;
+import org.junit.Test;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(Coders.DeflateDecoder.class)
 public class SevenZNativeHeapTest extends AbstractTestCase {
 
-    @InjectMocks
-    Coders.DeflateDecoder deflateDecoder;
 
     @Test
     public void testEndDeflaterOnCloseStream() throws Exception {
-        final Deflater deflater = PowerMockito.spy(new Deflater());
-        PowerMockito.whenNew(Deflater.class).withAnyArguments().thenReturn(deflater);
+        Coders.DeflateDecoder deflateDecoder = new DeflateDecoder();
 
-        final OutputStream outputStream = deflateDecoder.encode(new ByteArrayOutputStream(), 9);
+        final DeflateDecoderOutputStream outputStream =
+            (DeflateDecoderOutputStream) deflateDecoder.encode(new ByteArrayOutputStream(), 9);
+        DelegatingDeflater delegatingDeflater = new DelegatingDeflater(outputStream.deflater);
+        outputStream.deflater = delegatingDeflater;
         outputStream.close();
+        assertTrue(delegatingDeflater.isEnded.get());
 
-        Mockito.verify(deflater).end();
     }
 
     @Test
     public void testEndInflaterOnCloseStream() throws Exception {
-        final Inflater inflater = PowerMockito.spy(new Inflater());
-        PowerMockito.whenNew(Inflater.class).withAnyArguments().thenReturn(inflater);
-
-        final InputStream inputStream = deflateDecoder.decode("dummy",new ByteArrayInputStream(new byte[0]),0,null,null);
+        Coders.DeflateDecoder deflateDecoder = new DeflateDecoder();
+        final DeflateDecoderInputStream inputStream =
+            (DeflateDecoderInputStream) deflateDecoder.decode("dummy",new ByteArrayInputStream(new byte[0]),0,null,null);
+        DelegatingInflater delegatingInflater = new DelegatingInflater(inputStream.inflater);
+        inputStream.inflater = delegatingInflater;
         inputStream.close();
 
-        Mockito.verify(inflater).end();
+        assertTrue(delegatingInflater.isEnded.get());
+    }
+
+    private class DelegatingInflater extends Inflater {
+
+        private final Inflater inflater;
+
+        public DelegatingInflater(Inflater inflater) {
+            this.inflater = inflater;
+        }
+        AtomicBoolean isEnded = new AtomicBoolean();
+
+        @Override
+        public void end() {
+            isEnded.set(true);
+            inflater.end();
+        }
+
+        @Override
+        public void setInput(byte[] b, int off, int len) {
+            inflater.setInput(b, off, len);
+        }
+
+        @Override
+        public void setInput(byte[] b) {
+            inflater.setInput(b);
+        }
+
+        @Override
+        public void setDictionary(byte[] b, int off, int len) {
+            inflater.setDictionary(b, off, len);
+        }
+
+        @Override
+        public void setDictionary(byte[] b) {
+            inflater.setDictionary(b);
+        }
+
+        @Override
+        public int getRemaining() {
+            return inflater.getRemaining();
+        }
+
+        @Override
+        public boolean needsInput() {
+            return inflater.needsInput();
+        }
+
+        @Override
+        public boolean needsDictionary() {
+            return inflater.needsDictionary();
+        }
+
+        @Override
+        public boolean finished() {
+            return inflater.finished();
+        }
+
+        @Override
+        public int inflate(byte[] b, int off, int len) throws DataFormatException {
+            return inflater.inflate(b, off, len);
+        }
+
+        @Override
+        public int inflate(byte[] b) throws DataFormatException {
+            return inflater.inflate(b);
+        }
+
+        @Override
+        public int getAdler() {
+            return inflater.getAdler();
+        }
+
+        @Override
+        public int getTotalIn() {
+            return inflater.getTotalIn();
+        }
+
+        @Override
+        public long getBytesRead() {
+            return inflater.getBytesRead();
+        }
+
+        @Override
+        public int getTotalOut() {
+            return inflater.getTotalOut();
+        }
+
+        @Override
+        public long getBytesWritten() {
+            return inflater.getBytesWritten();
+        }
+
+        @Override
+        public void reset() {
+            inflater.reset();
+        }
+
+    }
+
+    private class DelegatingDeflater extends Deflater {
+
+        private final Deflater deflater;
+
+        public DelegatingDeflater(Deflater deflater) {
+            this.deflater = deflater;
+        }
+
+        AtomicBoolean isEnded = new AtomicBoolean();
+
+        @Override
+        public void end() {
+            isEnded.set(true);
+            deflater.end();
+        }
+
+        @Override
+        public void setInput(byte[] b, int off, int len) {
+            deflater.setInput(b, off, len);
+        }
+
+        @Override
+        public void setInput(byte[] b) {
+            deflater.setInput(b);
+        }
+
+        @Override
+        public void setDictionary(byte[] b, int off, int len) {
+            deflater.setDictionary(b, off, len);
+        }
+
+        @Override
+        public void setDictionary(byte[] b) {
+            deflater.setDictionary(b);
+        }
+
+        @Override
+        public void setStrategy(int strategy) {
+            deflater.setStrategy(strategy);
+        }
+
+        @Override
+        public void setLevel(int level) {
+            deflater.setLevel(level);
+        }
+
+        @Override
+        public boolean needsInput() {
+            return deflater.needsInput();
+        }
+
+        @Override
+        public void finish() {
+            deflater.finish();
+        }
+
+        @Override
+        public boolean finished() {
+            return deflater.finished();
+        }
+
+        @Override
+        public int deflate(byte[] b, int off, int len) {
+            return deflater.deflate(b, off, len);
+        }
+
+        @Override
+        public int deflate(byte[] b) {
+            return deflater.deflate(b);
+        }
+
+        @Override
+        public int deflate(byte[] b, int off, int len, int flush) {
+            return deflater.deflate(b, off, len, flush);
+        }
+
+        @Override
+        public int getAdler() {
+            return deflater.getAdler();
+        }
+
+        @Override
+        public int getTotalIn() {
+            return deflater.getTotalIn();
+        }
+
+        @Override
+        public long getBytesRead() {
+            return deflater.getBytesRead();
+        }
+
+        @Override
+        public int getTotalOut() {
+            return deflater.getTotalOut();
+        }
+
+        @Override
+        public long getBytesWritten() {
+            return deflater.getBytesWritten();
+        }
+
+        @Override
+        public void reset() {
+            deflater.reset();
+        }
+
+
     }
 }
