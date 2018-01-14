f43590aaf5416ce79cb1aad770c83096ddf2a5c2
shahab <shahab.kondri@gmail.com>
2018-01-14 14:37:59 -0500

    'long' literal ending with lowercase 'l' instead of 'L'

--- src/main/java/org/apache/commons/compress/archivers/tar/TarUtils.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarUtils.java
@@ -491,7 +491,7 @@ public class TarUtils {
                                          final int offset, final int length,
                                          final boolean negative) {
         final int bits = (length - 1) * 8;
-        final long max = 1l << bits;
+        final long max = 1L << bits;
         long val = Math.abs(value); // Long.MIN_VALUE stays Long.MIN_VALUE
         if (val < 0 || val >= max) {
             throw new IllegalArgumentException("Value " + value +
@@ -500,7 +500,7 @@ public class TarUtils {
         if (negative) {
             val ^= max - 1;
             val++;
-            val |= 0xffl << bits;
+            val |= 0xffL << bits;
         }
         for (int i = offset + length - 1; i >= offset; i--) {
             buf[i] = (byte) val;
