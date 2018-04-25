9146faa1d7f1375c50c2732f4a18fe955772a1b6
Stefan Bodewig <bodewig@apache.org>
2018-04-25 20:20:09 +0200

    COMPRESS-443 use native container and reference URI for bundle

--- src/test/java/org/apache/commons/compress/OsgiITest.java
+++ src/test/java/org/apache/commons/compress/OsgiITest.java
@@ -18,65 +18,35 @@
  */
 package org.apache.commons.compress;
 
-import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
-
-import java.io.File;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
-import org.ops4j.pax.exam.ConfigurationManager;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
-import org.ops4j.pax.exam.options.MavenUrlReference;
 
 @RunWith(PaxExam.class)
 public class OsgiITest {
 
     @Configuration
     public Option[] config() {
-        MavenArtifactUrlReference karafUrl = maven()
-            .groupId("org.apache.karaf")
-            .artifactId("apache-karaf")
-            .version(karafVersion())
-            .type("zip");
-
-        MavenUrlReference karafStandardRepo = maven()
-            .groupId("org.apache.karaf.features")
-            .artifactId("standard")
-            .version(karafVersion())
-            .classifier("features")
-            .type("xml");
         return new Option[] {
-            // KarafDistributionOption.debugConfiguration("5005", true),
-            karafDistributionConfiguration()
-                .frameworkUrl(karafUrl)
-                .unpackDirectory(new File("target", "exam"))
-                .useDeployFolder(false),
-            keepRuntimeFolder(),
-            configureConsole().ignoreLocalConsole(),
-            features(karafStandardRepo , "scr"),
-            mavenBundle()
-                .groupId("org.apache.commons")
-                .artifactId("commons-compress")
-                .version(commonsCompressVersion()).start(),
+            systemProperty("pax.exam.osgi.unresolved.fail").value("true"),
+            mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.scr")
+                .version("2.0.14"),
+            mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.configadmin")
+                .version("1.8.16"),
+            composite(systemProperty("pax.exam.invoker").value("junit"),
+                      bundle("link:classpath:META-INF/links/org.ops4j.pax.tipi.junit.link"),
+                      bundle("link:classpath:META-INF/links/org.ops4j.pax.exam.invoker.junit.link"),
+                      mavenBundle().groupId("org.apache.servicemix.bundles")
+                          .artifactId("org.apache.servicemix.bundles.hamcrest").version("1.3_1")),
+            bundle("reference:file:target/classes/").start()
        };
-    }
-
-    public static String karafVersion() {
-        ConfigurationManager cm = new ConfigurationManager();
-        String karafVersion = cm.getProperty("pax.exam.karaf.version", "3.0.0");
-        return karafVersion;
-    }
-
-    public static String commonsCompressVersion() {
-        return new ConfigurationManager().getProperty("commons-compress.version", "");
     }
 
     @Test
