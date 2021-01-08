a9eabd50c5d356f129ccebe032a4f47ccd654460
Gary Gregory <garydgregory@gmail.com>
2021-01-08 09:51:39 -0500

    Remove unused import.

--- src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
--- src/main/java/org/apache/commons/compress/archivers/zip/ZipSplitReadOnlySeekableByteChannel.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/ZipSplitReadOnlySeekableByteChannel.java
@@ -31,7 +31,6 @@ import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
