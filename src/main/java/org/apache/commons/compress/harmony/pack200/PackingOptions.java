2e4b0d67233a1547d038edce374fdb3a2bb513e8
Gary Gregory <garydgregory@gmail.com>
2021-12-02 19:11:50 -0500

    Sort members.

--- src/main/java/org/apache/commons/compress/harmony/pack200/PackingOptions.java
+++ src/main/java/org/apache/commons/compress/harmony/pack200/PackingOptions.java
@@ -53,149 +53,12 @@ public class PackingOptions {
 
     private Attribute[] unknownAttributeTypes;
 
-    public boolean isGzip() {
-        return gzip;
-    }
-
-    public void setGzip(final boolean gzip) {
-        this.gzip = gzip;
-    }
-
-    public boolean isStripDebug() {
-        return stripDebug;
-    }
-
-    /**
-     * Set strip debug attributes. If true, all debug attributes (i.e. LineNumberTable, SourceFile, LocalVariableTable
-     * and LocalVariableTypeTable attributes) are stripped when reading the input class files and not included in the
-     * output archive.
-     *
-     * @param stripDebug If true, all debug attributes.
-     */
-    public void setStripDebug(final boolean stripDebug) {
-        this.stripDebug = stripDebug;
-    }
-
-    public boolean isKeepFileOrder() {
-        return keepFileOrder;
-    }
-
-    public void setKeepFileOrder(final boolean keepFileOrder) {
-        this.keepFileOrder = keepFileOrder;
-    }
-
-    public long getSegmentLimit() {
-        return segmentLimit;
-    }
-
-    /**
-     * Set the segment limit (equivalent to -S command line option)
-     *
-     * @param segmentLimit - the limit in bytes
-     */
-    public void setSegmentLimit(final long segmentLimit) {
-        this.segmentLimit = segmentLimit;
-    }
-
-    public int getEffort() {
-        return effort;
-    }
-
-    /**
-     * Sets the compression effort level (0-9, equivalent to -E command line option)
-     *
-     * @param effort the compression effort level, 0-9.
-     */
-    public void setEffort(final int effort) {
-        this.effort = effort;
-    }
-
-    public String getDeflateHint() {
-        return deflateHint;
-    }
-
-    public boolean isKeepDeflateHint() {
-        return KEEP.equals(deflateHint);
-    }
-
-    public void setDeflateHint(final String deflateHint) {
-        if (!KEEP.equals(deflateHint) && !"true".equals(deflateHint) && !"false".equals(deflateHint)) {
-            throw new IllegalArgumentException(
-                "Bad argument: -H " + deflateHint + " ? deflate hint should be either true, false or keep (default)");
-        }
-        this.deflateHint = deflateHint;
-    }
-
-    public String getModificationTime() {
-        return modificationTime;
-    }
-
-    public void setModificationTime(final String modificationTime) {
-        if (!KEEP.equals(modificationTime) && !"latest".equals(modificationTime)) {
-            throw new IllegalArgumentException("Bad argument: -m " + modificationTime
-                + " ? transmit modtimes should be either latest or keep (default)");
-        }
-        this.modificationTime = modificationTime;
-    }
-
-    public boolean isPassFile(final String passFileName) {
-        for (String pass : passFiles) {
-            if (passFileName.equals(pass)) {
-                return true;
-            }
-            if (!pass.endsWith(".class")) { // a whole directory is
-                // passed
-                if (!pass.endsWith("/")) {
-                    // Make sure we don't get any false positives (e.g.
-                    // exclude "org/apache/harmony/pack" should not match
-                    // files under "org/apache/harmony/pack200/")
-                    pass = pass + "/";
-                }
-                return passFileName.startsWith(pass);
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Tell the compressor to pass the file with the given name, or if the name is a directory name all files under that
-     * directory will be passed.
-     *
-     * @param passFileName the file name
-     */
-    public void addPassFile(String passFileName) {
-        String fileSeparator = System.getProperty("file.separator");
-        if (fileSeparator.equals("\\")) {
-            // Need to escape backslashes for replaceAll(), which uses regex
-            fileSeparator += "\\";
-        }
-        passFileName = passFileName.replaceAll(fileSeparator, "/");
-        passFiles.add(passFileName);
-    }
-
-    public void removePassFile(final String passFileName) {
-        passFiles.remove(passFileName);
-    }
-
-    public String getUnknownAttributeAction() {
-        return unknownAttributeAction;
-    }
-
-    /**
-     * Tell the compressor what to do if an unknown attribute is encountered
-     *
-     * @param unknownAttributeAction - the action to perform
-     */
-    public void setUnknownAttributeAction(final String unknownAttributeAction) {
-        this.unknownAttributeAction = unknownAttributeAction;
-        if (!PASS.equals(unknownAttributeAction) && !ERROR.equals(unknownAttributeAction)
-            && !STRIP.equals(unknownAttributeAction)) {
-            throw new RuntimeException("Incorrect option for -U, " + unknownAttributeAction);
-        }
-    }
-
     public void addClassAttributeAction(final String attributeName, final String action) {
         classAttributeActions.put(attributeName, action);
+    }
+
+    public void addCodeAttributeAction(final String attributeName, final String action) {
+        codeAttributeActions.put(attributeName, action);
     }
 
     public void addFieldAttributeAction(final String attributeName, final String action) {
@@ -204,30 +67,6 @@ public class PackingOptions {
 
     public void addMethodAttributeAction(final String attributeName, final String action) {
         methodAttributeActions.put(attributeName, action);
-    }
-
-    public void addCodeAttributeAction(final String attributeName, final String action) {
-        codeAttributeActions.put(attributeName, action);
-    }
-
-    public boolean isVerbose() {
-        return verbose;
-    }
-
-    public void setVerbose(final boolean verbose) {
-        this.verbose = verbose;
-    }
-
-    public void setQuiet(final boolean quiet) {
-        this.verbose = !quiet;
-    }
-
-    public String getLogFile() {
-        return logFile;
-    }
-
-    public void setLogFile(final String logFile) {
-        this.logFile = logFile;
     }
 
     private void addOrUpdateAttributeActions(final List<Attribute> prototypes, final Map<String, String> attributeActions, final int tag) {
@@ -262,6 +101,46 @@ public class PackingOptions {
         }
     }
 
+    /**
+     * Tell the compressor to pass the file with the given name, or if the name is a directory name all files under that
+     * directory will be passed.
+     *
+     * @param passFileName the file name
+     */
+    public void addPassFile(String passFileName) {
+        String fileSeparator = System.getProperty("file.separator");
+        if (fileSeparator.equals("\\")) {
+            // Need to escape backslashes for replaceAll(), which uses regex
+            fileSeparator += "\\";
+        }
+        passFileName = passFileName.replaceAll(fileSeparator, "/");
+        passFiles.add(passFileName);
+    }
+
+    public String getDeflateHint() {
+        return deflateHint;
+    }
+
+    public int getEffort() {
+        return effort;
+    }
+
+    public String getLogFile() {
+        return logFile;
+    }
+
+    public String getModificationTime() {
+        return modificationTime;
+    }
+
+    public long getSegmentLimit() {
+        return segmentLimit;
+    }
+
+    public String getUnknownAttributeAction() {
+        return unknownAttributeAction;
+    }
+
     public Attribute[] getUnknownAttributePrototypes() {
         if (unknownAttributeTypes == null) {
             final List<Attribute> prototypes = new ArrayList<>();
@@ -281,11 +160,11 @@ public class PackingOptions {
         return classAttributeActions.getOrDefault(type, unknownAttributeAction);
     }
 
-    public String getUnknownMethodAttributeAction(final String type) {
-        if (methodAttributeActions == null) {
+    public String getUnknownCodeAttributeAction(final String type) {
+        if (codeAttributeActions == null) {
             return unknownAttributeAction;
         }
-        return methodAttributeActions.getOrDefault(type, unknownAttributeAction);
+        return codeAttributeActions.getOrDefault(type, unknownAttributeAction);
     }
 
     public String getUnknownFieldAttributeAction(final String type) {
@@ -295,11 +174,132 @@ public class PackingOptions {
         return fieldAttributeActions.getOrDefault(type, unknownAttributeAction);
     }
 
-    public String getUnknownCodeAttributeAction(final String type) {
-        if (codeAttributeActions == null) {
+    public String getUnknownMethodAttributeAction(final String type) {
+        if (methodAttributeActions == null) {
             return unknownAttributeAction;
         }
-        return codeAttributeActions.getOrDefault(type, unknownAttributeAction);
+        return methodAttributeActions.getOrDefault(type, unknownAttributeAction);
+    }
+
+    public boolean isGzip() {
+        return gzip;
+    }
+
+    public boolean isKeepDeflateHint() {
+        return KEEP.equals(deflateHint);
+    }
+
+    public boolean isKeepFileOrder() {
+        return keepFileOrder;
+    }
+
+    public boolean isPassFile(final String passFileName) {
+        for (String pass : passFiles) {
+            if (passFileName.equals(pass)) {
+                return true;
+            }
+            if (!pass.endsWith(".class")) { // a whole directory is
+                // passed
+                if (!pass.endsWith("/")) {
+                    // Make sure we don't get any false positives (e.g.
+                    // exclude "org/apache/harmony/pack" should not match
+                    // files under "org/apache/harmony/pack200/")
+                    pass = pass + "/";
+                }
+                return passFileName.startsWith(pass);
+            }
+        }
+        return false;
+    }
+
+    public boolean isStripDebug() {
+        return stripDebug;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void removePassFile(final String passFileName) {
+        passFiles.remove(passFileName);
+    }
+
+    public void setDeflateHint(final String deflateHint) {
+        if (!KEEP.equals(deflateHint) && !"true".equals(deflateHint) && !"false".equals(deflateHint)) {
+            throw new IllegalArgumentException(
+                "Bad argument: -H " + deflateHint + " ? deflate hint should be either true, false or keep (default)");
+        }
+        this.deflateHint = deflateHint;
+    }
+
+    /**
+     * Sets the compression effort level (0-9, equivalent to -E command line option)
+     *
+     * @param effort the compression effort level, 0-9.
+     */
+    public void setEffort(final int effort) {
+        this.effort = effort;
+    }
+
+    public void setGzip(final boolean gzip) {
+        this.gzip = gzip;
+    }
+
+    public void setKeepFileOrder(final boolean keepFileOrder) {
+        this.keepFileOrder = keepFileOrder;
+    }
+
+    public void setLogFile(final String logFile) {
+        this.logFile = logFile;
+    }
+
+    public void setModificationTime(final String modificationTime) {
+        if (!KEEP.equals(modificationTime) && !"latest".equals(modificationTime)) {
+            throw new IllegalArgumentException("Bad argument: -m " + modificationTime
+                + " ? transmit modtimes should be either latest or keep (default)");
+        }
+        this.modificationTime = modificationTime;
+    }
+
+    public void setQuiet(final boolean quiet) {
+        this.verbose = !quiet;
+    }
+
+    /**
+     * Set the segment limit (equivalent to -S command line option)
+     *
+     * @param segmentLimit - the limit in bytes
+     */
+    public void setSegmentLimit(final long segmentLimit) {
+        this.segmentLimit = segmentLimit;
+    }
+
+    /**
+     * Set strip debug attributes. If true, all debug attributes (i.e. LineNumberTable, SourceFile, LocalVariableTable
+     * and LocalVariableTypeTable attributes) are stripped when reading the input class files and not included in the
+     * output archive.
+     *
+     * @param stripDebug If true, all debug attributes.
+     */
+    public void setStripDebug(final boolean stripDebug) {
+        this.stripDebug = stripDebug;
+    }
+
+    /**
+     * Tell the compressor what to do if an unknown attribute is encountered
+     *
+     * @param unknownAttributeAction - the action to perform
+     */
+    public void setUnknownAttributeAction(final String unknownAttributeAction) {
+        this.unknownAttributeAction = unknownAttributeAction;
+        if (!PASS.equals(unknownAttributeAction) && !ERROR.equals(unknownAttributeAction)
+            && !STRIP.equals(unknownAttributeAction)) {
+            throw new RuntimeException("Incorrect option for -U, " + unknownAttributeAction);
+        }
+    }
+
+    public void setVerbose(final boolean verbose) {
+        this.verbose = verbose;
     }
 
 }
