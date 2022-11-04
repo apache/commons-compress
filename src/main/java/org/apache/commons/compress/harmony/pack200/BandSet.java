619d0a75bd8afa3bd1b0862f5135fb467bad8bae
Gary Gregory <garydgregory@gmail.com>
2022-11-04 18:15:51 -0400

    Organize imports

--- src/main/java/org/apache/commons/compress/harmony/pack200/BandSet.java
+++ src/main/java/org/apache/commons/compress/harmony/pack200/BandSet.java
@@ -23,8 +23,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
