17edab7e7444c07fecdf0b032eff974a2306edd6
Gary Gregory <garydgregory@users.noreply.github.com>
2023-06-16 13:59:40 -0400

    No need to abbreviate in doc.

--- src/main/java/org/apache/commons/compress/archivers/zip/ExtraFieldUtils.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/ExtraFieldUtils.java
@@ -25,7 +25,7 @@ import java.util.zip.ZipException;
 
 /**
  * ZipExtraField related methods
- * @NotThreadSafe because the HashMap is not sync.
+ * @NotThreadSafe because the HashMap is not synchronized.
  */
 // CheckStyle:HideUtilityClassConstructorCheck OFF (bc)
 public class ExtraFieldUtils {
