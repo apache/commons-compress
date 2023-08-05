484ae769388dd1652a2b65f875b4e567123bc32c
Gary Gregory <garydgregory@gmail.com>
2023-08-05 15:45:17 -0400

    Better JUnit API usage

--- src/test/java/org/apache/commons/compress/harmony/unpack200/tests/ArchiveTest.java
+++ src/test/java/org/apache/commons/compress/harmony/unpack200/tests/ArchiveTest.java
@@ -176,7 +176,7 @@ public class ArchiveTest {
         archive.setLogFile(logFile.getPath(), false);
         archive.setVerbose(true);
         archive.unpack();
-        assertTrue(logFile.length() == length);
+        assertEquals(logFile.length(), length);
 
         // test setting quiet explicitly
         in = Archive.class
