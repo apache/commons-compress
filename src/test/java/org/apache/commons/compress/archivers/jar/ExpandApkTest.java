149616fef1a397cd81ca5509dff07e041b6022e5
Gary Gregory <garydgregory@gmail.com>
2023-08-29 10:08:27 -0400

    Replace deprecated method call

--- src/test/java/org/apache/commons/compress/archivers/jar/ExpandApkTest.java
+++ src/test/java/org/apache/commons/compress/archivers/jar/ExpandApkTest.java
@@ -39,7 +39,7 @@ public class ExpandApkTest {
     @Test
     public void test() throws IOException {
         Streams
-            .stream(
+            .failableStream(
                 PathUtils.walk(Paths.get("src/test/resources/android"), new SuffixFileFilter("." + ArchiveStreamFactory.APK, IOCase.INSENSITIVE), 100, false))
             .forEach(path -> new Expander().expand(ArchiveStreamFactory.APK, println(path), null));
     }
