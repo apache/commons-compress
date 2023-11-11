69b66eadbc41b144b2fb445832d1cbca006a7b8a
Chad Preisler <4226832-chad.preisler@users.noreply.gitlab.com>
2023-11-11 10:57:13 -0600

    reverting changes.

--- src/test/java/org/apache/commons/compress/compressors/lz4/FramedLZ4CompressorOutputStreamTest.java
+++ src/test/java/org/apache/commons/compress/compressors/lz4/FramedLZ4CompressorOutputStreamTest.java
@@ -24,6 +24,10 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
+/**
+ *
+ * @author Chad Preisler
+ */
 public class FramedLZ4CompressorOutputStreamTest {
     
     @Test
