da84b77c32d77f69a9aa0eb427e3a9ba09490188
Gary Gregory <garydgregory@gmail.com>
2023-12-21 22:57:19 -0500

    Reuse commons-io, don't duplicate class BoundedInputStream (deprecated class)

--- src/main/java/org/apache/commons/compress/utils/BoundedInputStream.java
+++ src/main/java/org/apache/commons/compress/utils/BoundedInputStream.java
@@ -16,8 +16,6 @@
  */
 package org.apache.commons.compress.utils;
 
-import java.io.FilterInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -25,9 +23,10 @@ import java.io.InputStream;
  *
  * @NotThreadSafe
  * @since 1.6
+ * @deprecated Use {@link org.apache.commons.io.input.BoundedInputStream}.
  */
-public class BoundedInputStream extends FilterInputStream {
-    private long bytesRemaining;
+@Deprecated
+public class BoundedInputStream extends org.apache.commons.io.input.BoundedInputStream {
 
     /**
      * Creates the stream that will at most read the given amount of bytes from the given stream.
@@ -36,14 +35,8 @@ public class BoundedInputStream extends FilterInputStream {
      * @param size the maximum amount of bytes to read
      */
     public BoundedInputStream(final InputStream in, final long size) {
-        super(in);
-        bytesRemaining = size;
-    }
-
-    @Override
-    public void close() {
-        // there isn't anything to close in this stream and the nested
-        // stream is controlled externally
+        super(in, size);
+        setPropagateClose(false);
     }
 
     /**
@@ -53,46 +46,7 @@ public class BoundedInputStream extends FilterInputStream {
      * @since 1.21
      */
     public long getBytesRemaining() {
-        return bytesRemaining;
+        return getMaxLength() - getCount();
     }
 
-    @Override
-    public int read() throws IOException {
-        if (bytesRemaining > 0) {
-            --bytesRemaining;
-            return in.read();
-        }
-        return -1;
-    }
-
-    @Override
-    public int read(final byte[] b, final int off, final int len) throws IOException {
-        if (len == 0) {
-            return 0;
-        }
-        if (bytesRemaining == 0) {
-            return -1;
-        }
-        int bytesToRead = len;
-        if (bytesToRead > bytesRemaining) {
-            bytesToRead = (int) bytesRemaining;
-        }
-        final int bytesRead = in.read(b, off, bytesToRead);
-        if (bytesRead >= 0) {
-            bytesRemaining -= bytesRead;
-        }
-        return bytesRead;
-    }
-
-    /**
-     * @since 1.20
-     */
-    @Override
-    public long skip(final long n) throws IOException {
-        final long bytesToSkip = Math.min(bytesRemaining, n);
-        final long bytesSkipped = in.skip(bytesToSkip);
-        bytesRemaining -= bytesSkipped;
-
-        return bytesSkipped;
-    }
 }
