9f812ec9eaafa17eb8e54b8d52339c0bcbd839b8
Gary Gregory <garydgregory@gmail.com>
2024-01-31 08:23:12 -0500

    Don't use deprecated code here

--- src/test/java/org/apache/commons/compress/compressors/xz/XZCompressorInputStreamTest.java
+++ src/test/java/org/apache/commons/compress/compressors/xz/XZCompressorInputStreamTest.java
@@ -28,7 +28,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 
-import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 public class XZCompressorInputStreamTest {
