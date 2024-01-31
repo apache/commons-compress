33004fbaba3145e7342950c93c37b11dc5dd16d0
Gary Gregory <garydgregory@gmail.com>
2024-01-31 08:10:44 -0500

    Don't use deprecated code here

--- src/test/java/org/apache/commons/compress/archivers/cpio/CpioArchiveTest.java
+++ src/test/java/org/apache/commons/compress/archivers/cpio/CpioArchiveTest.java
@@ -27,7 +27,7 @@ import java.io.ByteArrayOutputStream;
 import java.util.stream.Stream;
 
 import org.apache.commons.compress.utils.CharsetNames;
-import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
