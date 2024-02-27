ee05c84c971c6133aa88f5993e78dfb6f8d768e2
Gary Gregory <garydgregory@gmail.com>
2024-02-27 17:38:44 -0500

    Inline singel-use local variable

--- src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
@@ -318,8 +318,7 @@ public class TarArchiveInputStream extends ArchiveInputStream<TarArchiveEntry> {
     private void consumeRemainderOfLastBlock() throws IOException {
         final long bytesReadOfLastBlock = getBytesRead() % blockSize;
         if (bytesReadOfLastBlock > 0) {
-            final long skipped = org.apache.commons.io.IOUtils.skip(in, blockSize - bytesReadOfLastBlock);
-            count(skipped);
+            count(org.apache.commons.io.IOUtils.skip(in, blockSize - bytesReadOfLastBlock));
         }
     }
 
