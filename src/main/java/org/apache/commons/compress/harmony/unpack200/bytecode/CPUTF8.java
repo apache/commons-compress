e0e1c2ba5013b20257388dcd7a2088492cd18633
Gary Gregory <garydgregory@gmail.com>
2024-05-18 08:42:36 -0400

    Javadoc

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPUTF8.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPUTF8.java
@@ -40,7 +40,7 @@ public class CPUTF8 extends ConstantPoolEntry {
      * Creates a new CPUTF8 instance
      *
      * @param utf8        TODO
-     * @param globalIndex - index in CpBands
+     * @param globalIndex index in CpBands
      * @throws NullPointerException if utf8 is null
      */
     public CPUTF8(final String utf8, final int globalIndex) {
