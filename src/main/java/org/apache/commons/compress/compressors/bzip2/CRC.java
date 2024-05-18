a43afa1e9dca6a60b29ad73242d99bc796d16fa9
Gary Gregory <garydgregory@gmail.com>
2024-05-18 08:02:56 -0400

    No need to qualify own name

--- src/main/java/org/apache/commons/compress/compressors/bzip2/CRC.java
+++ src/main/java/org/apache/commons/compress/compressors/bzip2/CRC.java
@@ -67,7 +67,7 @@ final class CRC {
         if (temp < 0) {
             temp = 256 + temp;
         }
-        crc = crc << 8 ^ CRC.crc32Table[temp];
+        crc = crc << 8 ^ crc32Table[temp];
     }
 
     void update(final int inCh, int repeat) {
