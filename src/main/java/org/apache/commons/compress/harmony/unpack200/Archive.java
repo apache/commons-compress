3f99ec3b50bb767ef725a9eb06fa42800e5c3354
Emmanuel Bourg <ebourg@apache.org>
2024-10-24 23:59:35 +0200

    Removed an unused import in the Archive class

--- src/main/java/org/apache/commons/compress/harmony/unpack200/Archive.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/Archive.java
@@ -23,7 +23,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
