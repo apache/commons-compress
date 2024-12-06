c1143e90a6cbf08189386e6ed9fd188bbaa81645
Gary Gregory <garydgregory@gmail.com>
2024-12-06 17:33:06 -0500

    Javadoc & in-line comment

--- src/main/java/org/apache/commons/compress/archivers/zip/UnixStat.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/UnixStat.java
@@ -20,40 +20,46 @@ package org.apache.commons.compress.archivers.zip;
 
 /**
  * Constants from stat.h on UNIX systems.
+ * <p>
+ * TODO This will be an class in 2.0.
+ * </p>
  */
 // CheckStyle:InterfaceIsTypeCheck OFF - backward compatible
 public interface UnixStat {
 
     /**
-     * Bits used for permissions (and sticky bit)
+     * Masks permissions (and sticky bit)
      */
     int PERM_MASK = 07777;
+
     /**
-     * Bits used to indicate the file system object type.
+     * Masks the file system object type.
      *
      * @since 1.14
      */
     int FILE_TYPE_FLAG = 0170000;
+
     /**
-     * Indicates symbolic links.
+     * Masks symbolic links.
      */
     int LINK_FLAG = 0120000;
+
     /**
-     * Indicates plain files.
+     * Masks plain files.
      */
     int FILE_FLAG = 0100000;
+
     /**
-     * Indicates directories.
+     * Masks directories.
      */
     int DIR_FLAG = 040000;
 
-    // ----------------------------------------------------------
-    // somewhat arbitrary choices that are quite common for shared
-    // installations
-    // -----------------------------------------------------------
+    //
+    // Arbitrary choices that are quite common for shared installations.
+    //
 
     /**
-     * Default permissions for symbolic links.
+     * Default permissions for all permissions.
      */
     int DEFAULT_LINK_PERM = 0777;
 
