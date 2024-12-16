242359980887142ff1b79efc22f259c89e2aa2a4
Gary D. Gregory <garydgregory@gmail.com>
2024-12-16 09:15:56 -0500

    Remove unused private instance variable

--- src/main/java/org/apache/commons/compress/archivers/cpio/CpioArchiveOutputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/cpio/CpioArchiveOutputStream.java
@@ -90,9 +90,6 @@ public class CpioArchiveOutputStream extends ArchiveOutputStream<CpioArchiveEntr
      */
     private final ZipEncoding zipEncoding;
 
-    // the provided encoding (for unit tests)
-    final String charsetName;
-
     /**
      * Constructs the cpio output stream. The format for this CPIO stream is the "new" format using ASCII encoding for file names
      *
@@ -148,7 +145,6 @@ public class CpioArchiveOutputStream extends ArchiveOutputStream<CpioArchiveEntr
         }
         this.entryFormat = format;
         this.blockSize = blockSize;
-        this.charsetName = encoding;
         this.zipEncoding = ZipEncodingHelper.getZipEncoding(encoding);
     }
 
