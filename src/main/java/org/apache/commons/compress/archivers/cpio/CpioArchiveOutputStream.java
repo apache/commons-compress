7e3c28c9387ce47f6d6ea6b1b025f5f857f6b421
Gary D. Gregory <garydgregory@gmail.com>
2024-12-16 15:40:31 -0500

    Revert "Remove unused private instance variable"
    This reverts commit 242359980887142ff1b79efc22f259c89e2aa2a4.

--- src/main/java/org/apache/commons/compress/archivers/cpio/CpioArchiveOutputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/cpio/CpioArchiveOutputStream.java
@@ -90,6 +90,9 @@ public class CpioArchiveOutputStream extends ArchiveOutputStream<CpioArchiveEntr
      */
     private final ZipEncoding zipEncoding;
 
+    // the provided encoding (for unit tests)
+    final String charsetName;
+
     /**
      * Constructs the cpio output stream. The format for this CPIO stream is the "new" format using ASCII encoding for file names
      *
@@ -145,6 +148,7 @@ public class CpioArchiveOutputStream extends ArchiveOutputStream<CpioArchiveEntr
         }
         this.entryFormat = format;
         this.blockSize = blockSize;
+        this.charsetName = encoding;
         this.zipEncoding = ZipEncodingHelper.getZipEncoding(encoding);
     }
 
