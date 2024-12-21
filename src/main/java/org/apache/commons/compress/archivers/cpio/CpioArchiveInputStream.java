646c459353f0683acc50eee8c688ecdaf93ab371
Gary D. Gregory <garydgregory@gmail.com>
2024-12-21 10:04:08 -0500

    Upper case acronym

--- src/main/java/org/apache/commons/compress/archivers/cpio/CpioArchiveInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/cpio/CpioArchiveInputStream.java
@@ -31,7 +31,7 @@ import org.apache.commons.compress.utils.ParsingUtils;
 
 /**
  * CpioArchiveInputStream is a stream for reading cpio streams. All formats of cpio are supported (old ascii, old binary, new portable format and the new
- * portable format with crc).
+ * portable format with CRC).
  * <p>
  * The stream can be read by extracting a cpio entry (containing all information about an entry) and afterwards reading from the stream the file specified by
  * the entry.
