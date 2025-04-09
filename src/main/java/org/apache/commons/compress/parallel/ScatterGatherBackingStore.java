ec81f8868d2218d4e62dcf0f0840ec27f270b11d
Gary Gregory <garydgregory@gmail.com>
2025-04-09 13:47:49 -0400

    Javadoc

--- src/main/java/org/apache/commons/compress/parallel/ScatterGatherBackingStore.java
+++ src/main/java/org/apache/commons/compress/parallel/ScatterGatherBackingStore.java
@@ -23,11 +23,8 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * <p>
  * Store intermediate payload in a scatter-gather scenario. Multiple threads write their payload to a backing store, which can subsequently be reversed to an
  * {@link InputStream} to be used as input in the gather phase.
- * </p>
- *
  * <p>
  * It is the responsibility of the allocator of an instance of this class to close this. Closing it should clear off any allocated structures and preferably
  * delete files.
@@ -40,7 +37,7 @@ public interface ScatterGatherBackingStore extends Closeable {
     /**
      * Closes this backing store for further writing.
      *
-     * @throws IOException when something fails
+     * @throws IOException if an I/O error occurs.
      */
     void closeForWriting() throws IOException;
 
@@ -48,7 +45,7 @@ public interface ScatterGatherBackingStore extends Closeable {
      * An input stream that contains the scattered payload
      *
      * @return An InputStream, should be closed by the caller of this method.
-     * @throws IOException when something fails
+     * @throws IOException if an I/O error occurs.
      */
     InputStream getInputStream() throws IOException;
 
@@ -58,7 +55,7 @@ public interface ScatterGatherBackingStore extends Closeable {
      * @param data   the data to write
      * @param offset offset inside data to start writing from
      * @param length the amount of data to write
-     * @throws IOException when something fails
+     * @throws IOException if an I/O error occurs.
      */
     void writeOut(byte[] data, int offset, int length) throws IOException;
 }
