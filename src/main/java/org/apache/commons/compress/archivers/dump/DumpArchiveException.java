aec630f136df01cb7b6e8afa43d3411112943a79
Gary Gregory <garydgregory@gmail.com>
2025-04-09 09:58:42 -0400

    Javadoc

--- src/main/java/org/apache/commons/compress/archivers/dump/DumpArchiveException.java
+++ src/main/java/org/apache/commons/compress/archivers/dump/DumpArchiveException.java
@@ -16,28 +16,57 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.commons.compress.archivers.dump;
 
 import java.io.IOException;
 
 /**
- * Dump Archive Exception
+ * Signals that an dump archive exception of some sort has occurred.
  */
 public class DumpArchiveException extends IOException {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Constructs a {@code DumpArchiveException} with {@code null} as its error detail message.
+     */
     public DumpArchiveException() {
     }
 
-    public DumpArchiveException(final String msg) {
-        super(msg);
+    /**
+     * Constructs a {@code DumpArchiveException} with the specified detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+     */
+    public DumpArchiveException(final String message) {
+        super(message);
     }
 
-    public DumpArchiveException(final String msg, final Throwable cause) {
-        super(msg, cause);
+    /**
+     * Constructs a {@code DumpArchiveException} with the specified detail message and cause.
+     *
+     * <p>
+     * Note that the detail message associated with {@code cause} is <em>not</em> automatically incorporated into this exception's detail message.
+     * </p>
+     *
+     * @param message The detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+     *
+     * @param cause   The cause (which is saved for later retrieval by the {@link #getCause()} method). (A null value is permitted, and indicates that the cause
+     *                is nonexistent or unknown.)
+     */
+    public DumpArchiveException(final String message, final Throwable cause) {
+        super(message, cause);
     }
 
+    /**
+     * Constructs a {@code DumpArchiveException} with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())} (which
+     * typically contains the class and detail message of {@code cause}). This constructor is useful for IO exceptions that are little more than wrappers for
+     * other throwables.
+     *
+     * @param cause The cause (which is saved for later retrieval by the {@link #getCause()} method). (A null value is permitted, and indicates that the cause
+     *              is nonexistent or unknown.)
+     */
     public DumpArchiveException(final Throwable cause) {
         super(cause);
     }
