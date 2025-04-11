53334220c8b5d80cb273e4bf09692e5b76e60f39
Gary D. Gregory <garydgregory@gmail.com>
2025-04-11 09:40:09 -0400

    Javadoc

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/ConstantPoolEntry.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/ConstantPoolEntry.java
@@ -22,18 +22,33 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 /**
- * Abstract superclass for constant pool entries
+ * Abstracts constant pool entries.
  */
 public abstract class ConstantPoolEntry extends ClassFileEntry {
 
+    /**
+     * The constant {@value} for a constant pool class.
+     */
     public static final byte CP_Class = 7;
 
+    /**
+     * The constant {@value} for a constant pool double.
+     */
     public static final byte CP_Double = 6;
 
+    /**
+     * The constant {@value} for a constant pool field reference.
+     */
     public static final byte CP_Fieldref = 9;
 
+    /**
+     * The constant {@value} for a constant pool float.
+     */
     public static final byte CP_Float = 4;
 
+    /**
+     * The constant {@value} for a constant pool int.
+     */
     public static final byte CP_Integer = 3;
 
     /*
@@ -41,20 +56,41 @@ public abstract class ConstantPoolEntry extends ClassFileEntry {
      * writeBody(DataOutputStream dos) throws IOException { dos.writeShort(index); } }
      */
 
+    /**
+     * The constant {@value} for a constant pool interface method reference.
+     */
     public static final byte CP_InterfaceMethodref = 11;
 
+    /**
+     * The constant {@value} for a constant pool long.
+     */
     public static final byte CP_Long = 5;
 
+    /**
+     * The constant {@value} for a constant pool method reference.
+     */
     public static final byte CP_Methodref = 10;
 
+    /**
+     * The constant {@value} for a constant pool name and type.
+     */
     public static final byte CP_NameAndType = 12;
 
+    /**
+     * The constant {@value} for a constant pool string.
+     */
     public static final byte CP_String = 8;
 
+    /**
+     * The constant {@value} for a constant pool UTF8.
+     */
     public static final byte CP_UTF8 = 1;
 
     byte tag;
 
+    /**
+     * Global index.
+     */
     protected int globalIndex;
 
     ConstantPoolEntry(final byte tag, final int globalIndex) {
@@ -71,10 +107,20 @@ public abstract class ConstantPoolEntry extends ClassFileEntry {
     @Override
     public abstract boolean equals(Object obj);
 
+    /**
+     * Gets the global index.
+     *
+     * @return the global index.
+     */
     public int getGlobalIndex() {
         return globalIndex;
     }
 
+    /**
+     * Gets the tag.
+
+     * @return the tag.
+     */
     public byte getTag() {
         return tag;
     }
@@ -82,5 +128,11 @@ public abstract class ConstantPoolEntry extends ClassFileEntry {
     @Override
     public abstract int hashCode();
 
+    /**
+     * Writes this instance to the given output stream.
+     *
+     * @param dos the output stream.
+     * @throws IOException if an I/O error occurs.
+     */
     protected abstract void writeBody(DataOutputStream dos) throws IOException;
 }
