b67df95f617073dbd75a82b83bf130befd1c2982
Gary D. Gregory <garydgregory@gmail.com>
2025-04-11 10:29:39 -0400

    Javadoc

--- src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/RuntimeVisibleorInvisibleParameterAnnotationsAttribute.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/RuntimeVisibleorInvisibleParameterAnnotationsAttribute.java
@@ -35,10 +35,20 @@ public class RuntimeVisibleorInvisibleParameterAnnotationsAttribute extends Anno
 
         private final Annotation[] annotations;
 
+        /**
+         * Constructs a new instance.
+         *
+         * @param annotations Annotation.
+         */
         public ParameterAnnotation(final Annotation[] annotations) {
             this.annotations = annotations;
         }
 
+        /**
+         * Gets all annotation class file entries.
+         *
+         * @return all annotation class file entries.
+         */
         public List<Object> getClassFileEntries() {
             final List<Object> nested = new ArrayList<>();
             for (final Annotation annotation : annotations) {
@@ -47,6 +57,11 @@ public class RuntimeVisibleorInvisibleParameterAnnotationsAttribute extends Anno
             return nested;
         }
 
+        /**
+         * Gets the cumulative length of all annotations.
+         *
+         * @return the cumulative length of all annotations.
+         */
         public int getLength() {
             int length = 2;
             for (final Annotation annotation : annotations) {
@@ -55,12 +70,23 @@ public class RuntimeVisibleorInvisibleParameterAnnotationsAttribute extends Anno
             return length;
         }
 
+        /**
+         * Resolves all annotations in this instance against the given pool.
+         *
+         * @param pool A class constant pool.
+         */
         public void resolve(final ClassConstantPool pool) {
             for (final Annotation annotation : annotations) {
                 annotation.resolve(pool);
             }
         }
 
+        /**
+         * Writes this body to the given output stream.
+         *
+         * @param dos the output stream.
+         * @throws IOException if an I/O error occurs.
+         */
         public void writeBody(final DataOutputStream dos) throws IOException {
             dos.writeShort(annotations.length);
             for (final Annotation annotation : annotations) {
