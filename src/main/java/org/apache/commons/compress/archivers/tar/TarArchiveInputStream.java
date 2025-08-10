fb113c9460f5207b067f1ef1e18408f3cdc5eae5
Piotr P. Karwasz <pkarwasz-github@apache.org>
2025-08-10 11:58:57 +0200

    fix: Remove unused private method

--- src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
+++ src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveInputStream.java
@@ -226,11 +226,6 @@ public class TarArchiveInputStream extends ArchiveInputStream<TarArchiveEntry> {
         this(inputStream, TarConstants.DEFAULT_BLKSIZE, TarConstants.DEFAULT_RCDSIZE, encoding);
     }
 
-    private void applyPaxHeadersToCurrentEntry(final Map<String, String> headers, final List<TarArchiveStructSparse> sparseHeaders) throws IOException {
-        currEntry.updateEntryFromPaxHeaders(headers);
-        currEntry.setSparseHeaders(sparseHeaders);
-    }
-
     /**
      * Gets the available data that can be read from the current entry in the archive. This does not indicate how much data is left in the entire archive, only
      * in the current entry. This value is determined from the entry's size header field and the amount of data already read from the current entry.
