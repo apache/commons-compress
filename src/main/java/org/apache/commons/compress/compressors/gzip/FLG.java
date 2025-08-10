fc409b152e64f3122ff5afed8f633a7ed0bfea66
Gary D. Gregory <garydgregory@gmail.com>
2025-08-10 10:58:31 -0400

    Javadoc

--- src/main/java/org/apache/commons/compress/compressors/gzip/FLG.java
+++ src/main/java/org/apache/commons/compress/compressors/gzip/FLG.java
@@ -20,8 +20,8 @@
 package org.apache.commons.compress.compressors.gzip;
 
 /**
- * Declares constants for the header {@code FLG} (FLaGs) byte from <a href="https://datatracker.ietf.org/doc/html/rfc1952">RFC 1952 GZIP File Format
- * Specification May 1996</a> section 2.3.1. Member header and trailer.
+ * Declares constants for the header {@code FLG} (FLaGs) byte from <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File
+ * Format Specification May 1996</a> section 2.3.1. Member header and trailer.
  * <p>
  * The {@code FLG} (FLaGs) byte is divided into individual bits as follows:
  * </p>
@@ -38,6 +38,8 @@ package org.apache.commons.compress.compressors.gzip;
  *     bit 6   reserved
  *     bit 7   reserved
  * </pre>
+ *
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File Format Specification section 2.3.1</a>
  */
 final class FLG {
 
@@ -47,11 +49,15 @@ final class FLG {
      * If set, a zero-terminated file comment is present, not interpreted, and only intended for human consumption. The comment must contain ISO 8859-1
      * (LATIN-1) characters. Line breaks should be encoded as a single line feed character (10 decimal).
      * </p>
+     *
+     * @see <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File Format Specification section 2.3.1</a>
      */
     static final int FCOMMENT = 0x10;
 
     /**
      * Header flag indicating an EXTRA subfields collection follows the header.
+     *
+     * @see <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File Format Specification section 2.3.1</a>
      */
     static final int FEXTRA = 0x04;
 
@@ -64,16 +70,22 @@ final class FLG {
      * <p>
      * The FHCRC bit was never set by versions of gzip up to 1.2.4, even though it was documented with a different meaning in gzip 1.2.4.
      * </p>
+     *
+     * @see <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File Format Specification section 2.3.1</a>
      */
     static final int FHCRC = 0x02;
 
     /**
      * Header flag indicating a file name follows the header.
+     *
+     * @see <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File Format Specification section 2.3.1</a>
      */
     static final int FNAME = 0x08;
 
     /**
      * Reserved {@code FLG} bits must be zero.
+     *
+     * @see <a href="https://datatracker.ietf.org/doc/html/rfc1952#section-2.3.1">RFC 1952 GZIP File Format Specification section 2.3.1</a>
      */
     static final int FRESERVED = 0xE0;
 
