5597ca046ae8363f2600ae63733128d3b49e93c0
Gary Gregory <garydgregory@gmail.com>
2025-12-21 17:57:02 -0500

    Deprecate org.apache.commons.compress.utils.OsgiUtils.OsgiUtils()

--- src/main/java/org/apache/commons/compress/utils/OsgiUtils.java
+++ src/main/java/org/apache/commons/compress/utils/OsgiUtils.java
@@ -58,4 +58,14 @@ public class OsgiUtils {
         return inOsgiEnvironment;
     }
 
+    /**
+     * Constructs a new instance.
+     *
+     * @deprecated Will be removed in 4.0.
+     */
+    @Deprecated
+    public OsgiUtils() {
+        // Utility class
+    }
+
 }
