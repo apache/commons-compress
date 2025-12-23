67160d7992af87721cf7e9a01bd87fd88cbce6d3
Gary Gregory <garydgregory@gmail.com>
2025-12-23 18:41:44 -0500

    Javadoc

--- src/main/java/org/apache/commons/compress/utils/FileNameUtils.java
+++ src/main/java/org/apache/commons/compress/utils/FileNameUtils.java
@@ -40,7 +40,7 @@ public class FileNameUtils {
      * Will return the file name itself if it doesn't contain any dots. All leading directories of the {@code file name} parameter are skipped.
      * </p>
      *
-     * @return the base name of file name
+     * @return the base name of file name.
      * @param path the path of the file to obtain the base name of.
      * @since 1.22
      * @deprecated Use {@link PathUtils#getBaseName(Path)}.
@@ -56,7 +56,7 @@ public class FileNameUtils {
      * Will return the file name itself if it doesn't contain any dots. All leading directories of the {@code file name} parameter are skipped.
      * </p>
      *
-     * @return the base name of file name
+     * @return the base name of file name.
      * @param fileName the name of the file to obtain the base name of.
      * @deprecated Use {@link FilenameUtils#removeExtension(String)}.
      */
@@ -75,7 +75,7 @@ public class FileNameUtils {
      * directories of the {@code file name} parameter are skipped.
      * </p>
      *
-     * @return the extension of file name
+     * @return the extension of file name.
      * @param path the path of the file to obtain the extension of.
      * @since 1.22
      * @deprecated Use {@link PathUtils#getExtension(Path)}.
@@ -92,7 +92,7 @@ public class FileNameUtils {
      * directories of the {@code fileName} parameter are skipped.
      * </p>
      *
-     * @return the extension of file name
+     * @return the extension of file name.
      * @param fileName the name of the file to obtain the extension of.
      * @deprecated Use {@link FilenameUtils#getExtension(String)}.
      */
