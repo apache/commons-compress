cf5ea3507fbd4961f10d05e6327be24eee335f29
Gary Gregory <garydgregory@gmail.com>
2025-12-23 18:41:44 -0500

    Javadoc

--- src/main/java/org/apache/commons/compress/utils/Iterators.java
+++ src/main/java/org/apache/commons/compress/utils/Iterators.java
@@ -37,10 +37,10 @@ public class Iterators {
      * When this method returns, the {@code iterator} will be "empty": its {@code hasNext()} method returns {@code false}.
      * </p>
      *
-     * @param <T>        type of the elements contained inside the collection
-     * @param collection target collection
-     * @param iterator   source
-     * @return {@code true} if the target {@code collection} was modified as a result of this operation
+     * @param <T>        type of the elements contained inside the collection.
+     * @param collection target collection.
+     * @param iterator   source.
+     * @return {@code true} if the target {@code collection} was modified as a result of this operation.
      */
     public static <T> boolean addAll(final Collection<T> collection, final Iterator<? extends T> iterator) {
         Objects.requireNonNull(collection);
