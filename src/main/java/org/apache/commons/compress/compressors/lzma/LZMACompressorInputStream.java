2d3a3302ff40e87f0a009b09bf7ba3b0c8325aa5
Gary Gregory <garydgregory@gmail.com>
2025-12-23 18:41:36 -0500

    Javadoc

--- src/main/java/org/apache/commons/compress/compressors/lzma/LZMACompressorInputStream.java
+++ src/main/java/org/apache/commons/compress/compressors/lzma/LZMACompressorInputStream.java
@@ -75,7 +75,7 @@ public class LZMACompressorInputStream extends CompressorInputStream implements 
         /**
          * Sets a working memory threshold in kibibytes (KiB).
          *
-         * @param memoryLimitKiB Sets a working memory threshold in kibibytes (KiB). Processing throws MemoryLimitException if memory use is above this
+         * @param memoryLimitKiB Sets a working memory threshold in kibibytes (KiB). Processing throws MemoryLimitException if memory use is above this.
          *                       threshold.
          * @return {@code this} instance.
          */
@@ -98,9 +98,9 @@ public class LZMACompressorInputStream extends CompressorInputStream implements 
     /**
      * Checks if the signature matches what is expected for an LZMA file.
      *
-     * @param signature the bytes to check
-     * @param length    the number of bytes to check
-     * @return true, if this stream is an LZMA compressed stream, false otherwise
+     * @param signature the bytes to check.
+     * @param length    the number of bytes to check.
+     * @return true, if this stream is an LZMA compressed stream, false otherwise.
      * @since 1.10
      */
     public static boolean matches(final byte[] signature, final int length) {
@@ -124,9 +124,9 @@ public class LZMACompressorInputStream extends CompressorInputStream implements 
     /**
      * Creates a new input stream that decompresses LZMA-compressed data from the specified input stream.
      *
-     * @param inputStream where to read the compressed data
+     * @param inputStream where to read the compressed data.
      * @throws IOException if the input is not in the .lzma format, the input is corrupt or truncated, the .lzma headers specify sizes that are not supported by
-     *                     this implementation, or the underlying {@code inputStream} throws an exception
+     *                     this implementation, or the underlying {@code inputStream} throws an exception.
      */
     public LZMACompressorInputStream(final InputStream inputStream) throws IOException {
         this(builder().setInputStream(inputStream));
@@ -135,10 +135,10 @@ public class LZMACompressorInputStream extends CompressorInputStream implements 
     /**
      * Creates a new input stream that decompresses LZMA-compressed data from the specified input stream.
      *
-     * @param inputStream     where to read the compressed data
+     * @param inputStream     where to read the compressed data.
      * @param memoryLimitKiB Sets a working memory threshold in kibibytes (KiB). Processing throws MemoryLimitException if memory use is above this threshold.
      * @throws IOException if the input is not in the .lzma format, the input is corrupt or truncated, the .lzma headers specify sizes that are not supported by
-     *                     this implementation, or the underlying {@code inputStream} throws an exception
+     *                     this implementation, or the underlying {@code inputStream} throws an exception.
      *
      * @since 1.14
      * @deprecated Use {@link #builder()}.
