bcdea1e4c6ad38838385ba974f0fe04c0fc78824
Gary Gregory <garydgregory@gmail.com>
2025-12-23 18:41:23 -0500

    Javadoc

--- src/main/java/org/apache/commons/compress/archivers/zip/CircularBuffer.java
+++ src/main/java/org/apache/commons/compress/archivers/zip/CircularBuffer.java
@@ -55,8 +55,8 @@ final class CircularBuffer {
     /**
      * Copies a previous interval in the buffer to the current position.
      *
-     * @param distance the distance from the current write position
-     * @param length   the number of bytes to copy
+     * @param distance the distance from the current write position.
+     * @param length   the number of bytes to copy.
      */
     public void copy(final int distance, final int length) {
         final int pos1 = writeIndex - distance;
