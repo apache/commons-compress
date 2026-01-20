8b7fb03f3ba0543fe7a26d48b09d4ece4c6bbf6d
Gary Gregory <garydgregory@users.noreply.github.com>
2026-01-20 16:52:07 -0500

    Use camel-case

--- src/test/java/org/apache/commons/compress/archivers/cpio/CpioArchiveEntryTest.java
+++ src/test/java/org/apache/commons/compress/archivers/cpio/CpioArchiveEntryTest.java
@@ -30,13 +30,13 @@ class CpioArchiveEntryTest {
 
     @ParameterizedTest
     @ValueSource(shorts = {CpioConstants.FORMAT_NEW, CpioConstants.FORMAT_NEW_CRC, CpioConstants.FORMAT_OLD_BINARY})
-    void testCpioEntrySize_notOver4GiB_nonOldAsciiFormat(short format) {
+    void testCpioEntrySizeUnder4GiBNotOldAsciiFormat(short format) {
         final CpioArchiveEntry entry = new CpioArchiveEntry(format);
         assertThrows(IllegalArgumentException.class, () -> entry.setSize(0x1FFFFFFFFL));
     }
 
     @Test
-    void testCpioEntrySize_oldAsciiFormat_allowsOver4GiB() {
+    void testCpioEntrySizeOldAsciiFormatOver4GiB() {
         final CpioArchiveEntry entry = new CpioArchiveEntry(CpioConstants.FORMAT_OLD_ASCII);
         entry.setSize(0x1FFFFFFFFL);
     }
