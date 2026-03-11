ed428a55d9e898a1b132c7c5578eb8b9213ba318
tallison <tallison@apache.org>
2026-03-11 15:19:38 -0400

    COMPRESS-721 -- avoid reflection when possible.

--- src/main/java/org/apache/commons/compress/harmony/unpack200/Pack200UnpackerAdapter.java
+++ src/main/java/org/apache/commons/compress/harmony/unpack200/Pack200UnpackerAdapter.java
@@ -59,8 +59,26 @@ public class Pack200UnpackerAdapter extends Pack200Adapter implements Unpacker {
         return newBoundedInputStream(file.toPath());
     }
 
+    @SuppressWarnings("resource") // Caller closes.
     private static BoundedInputStream newBoundedInputStream(final FileInputStream fileInputStream) throws IOException {
-        return newBoundedInputStream(readPathString(fileInputStream));
+        final String pathString = readPathString(fileInputStream);
+        if (pathString != null) {
+            return newBoundedInputStream(pathString);
+        }
+        // Reflection failed (e.g., Java 17+ strong encapsulation), fall back to channel size.
+        try {
+            final long size = fileInputStream.getChannel().size();
+            // @formatter:off
+            return BoundedInputStream.builder()
+                    .setInputStream(new BufferedInputStream(fileInputStream))
+                    .setMaxCount(size)
+                    .setPropagateClose(false)
+                    .get();
+            // @formatter:on
+        } catch (final IOException e) {
+            // No limit
+            return BoundedInputStream.builder().setInputStream(new BufferedInputStream(fileInputStream)).get();
+        }
     }
 
     @SuppressWarnings("resource") // Caller closes.
@@ -74,7 +92,10 @@ public class Pack200UnpackerAdapter extends Pack200Adapter implements Unpacker {
             return newBoundedInputStream(BoundedInputStream.builder().setInputStream(inputStream).get());
         }
         if (inputStream instanceof FilterInputStream) {
-            return newBoundedInputStream(unwrap((FilterInputStream) inputStream));
+            final InputStream unwrapped = unwrap((FilterInputStream) inputStream);
+            if (unwrapped != null) {
+                return newBoundedInputStream(unwrapped);
+            }
         }
         if (inputStream instanceof FileInputStream) {
             return newBoundedInputStream((FileInputStream) inputStream);
